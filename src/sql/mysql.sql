49653034f99f756b5613bb79a25a2c379a2ea3b7
Ayhan Akilli <info@ayhan.biz>
2016-04-07 16:05:29 +0200

    Remove attributes from entity skeleton and update SQL setup

--- src/sql/mysql.sql
+++ src/sql/mysql.sql
@@ -18,28 +18,20 @@ CREATE TABLE IF NOT EXISTS `account` (
     `role_id` INTEGER(11) NOT NULL,
     `is_active` BOOLEAN NOT NULL DEFAULT '0',
     `is_system` BOOLEAN NOT NULL DEFAULT '0',
-    `created` DATETIME NOT NULL,
-    `creator` INTEGER(11) DEFAULT NULL,
-    `modified` DATETIME NOT NULL,
-    `modifier` INTEGER(11) DEFAULT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `uni_account_name` (`name`),
     KEY `idx_account_role` (`role_id`),
     KEY `idx_account_active` (`is_active`),
-    KEY `idx_account_system` (`is_system`),
-    KEY `idx_account_created` (`created`),
-    KEY `idx_account_creator` (`creator`),
-    KEY `idx_account_modified` (`modified`),
-    KEY `idx_account_modifier` (`modifier`)
+    KEY `idx_account_system` (`is_system`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 --
 -- Data for table `account`
 --
 
-INSERT INTO `account` (`id`, `name`, `password`, `role_id`, `is_active`, `is_system`, `created`, `creator`, `modified`, `modifier`) VALUES
-(0, 'Anonymous', '', 0, '1', '1', NOW(), NULL, NOW(), NULL),
-(1, 'admin', '$2y$10$9wnkOfY1qLvz0sRXG5G.d.rf2NhCU8a9m.XrLYIgeQA.SioSWwtsW', 1, '1', '1', NOW(), NULL, NOW(), NULL);
+INSERT INTO `account` (`id`, `name`, `password`, `role_id`, `is_active`, `is_system`) VALUES
+(0, 'Anonymous', '', 0, '1', '1'),
+(1, 'admin', '$2y$10$9wnkOfY1qLvz0sRXG5G.d.rf2NhCU8a9m.XrLYIgeQA.SioSWwtsW', 1, '1', '1');
 
 -- --------------------------------------------------------
 
@@ -56,18 +48,10 @@ CREATE TABLE IF NOT EXISTS `attribute` (
     `foreign_entity_id` VARCHAR(100) DEFAULT NULL,
     `options_callback` VARCHAR(255) DEFAULT NULL,
     `options` TEXT DEFAULT NULL,
-    `created` DATETIME NOT NULL,
-    `creator` INTEGER(11) DEFAULT NULL,
-    `modified` DATETIME NOT NULL,
-    `modifier` INTEGER(11) DEFAULT NULL,
     PRIMARY KEY (`id`),
     KEY `idx_attribute_name` (`name`),
     KEY `idx_attribute_type` (`type`),
-    KEY `idx_attribute_foreign` (`foreign_entity_id`),
-    KEY `idx_attribute_created` (`created`),
-    KEY `idx_attribute_creator` (`creator`),
-    KEY `idx_attribute_modified` (`modified`),
-    KEY `idx_attribute_modifier` (`modifier`)
+    KEY `idx_attribute_foreign` (`foreign_entity_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -115,19 +99,11 @@ CREATE TABLE IF NOT EXISTS `eav` (
     `value_int` INTEGER(11) DEFAULT NULL,
     `value_text` TEXT DEFAULT NULL,
     `value_varchar` VARCHAR(255) DEFAULT NULL,
-    `created` DATETIME NOT NULL,
-    `creator` INTEGER(11) DEFAULT NULL,
-    `modified` DATETIME NOT NULL,
-    `modifier` INTEGER(11) DEFAULT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `uni_eav_value` (`attribute_id`, `content_id`),
     KEY `idx_eav_entity` (`entity_id`),
     KEY `idx_eav_attribute` (`attribute_id`),
-    KEY `idx_eav_content` (`content_id`),
-    KEY `idx_eav_created` (`created`),
-    KEY `idx_eav_creator` (`creator`),
-    KEY `idx_eav_modified` (`modified`),
-    KEY `idx_eav_modifier` (`modifier`)
+    KEY `idx_eav_content` (`content_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -145,19 +121,11 @@ CREATE TABLE IF NOT EXISTS `entity` (
     `toolbar` VARCHAR(255) NOT NULL,
     `sort_order` INTEGER(11) NOT NULL DEFAULT '0',
     `is_system` BOOLEAN NOT NULL DEFAULT '0',
-    `created` DATETIME NOT NULL,
-    `creator` INTEGER(11) DEFAULT NULL,
-    `modified` DATETIME NOT NULL,
-    `modifier` INTEGER(11) DEFAULT NULL,
     PRIMARY KEY (`id`),
     KEY `idx_entity_name` (`name`),
     KEY `idx_entity_toolbar` (`toolbar`),
     KEY `idx_entity_sort` (`sort_order`),
-    KEY `idx_entity_system` (`is_system`),
-    KEY `idx_entity_created` (`created`),
-    KEY `idx_entity_creator` (`creator`),
-    KEY `idx_entity_modified` (`modified`),
-    KEY `idx_entity_modifier` (`modifier`)
+    KEY `idx_entity_system` (`is_system`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -175,10 +143,6 @@ CREATE TABLE IF NOT EXISTS `menu` (
     `lft` INTEGER(11) NOT NULL,
     `rgt` INTEGER(11) NOT NULL,
     `is_system` BOOLEAN NOT NULL DEFAULT '0',
-    `created` DATETIME NOT NULL,
-    `creator` INTEGER(11) DEFAULT NULL,
-    `modified` DATETIME NOT NULL,
-    `modifier` INTEGER(11) DEFAULT NULL,
     PRIMARY KEY (`id`),
     KEY `idx_menu_name` (`name`),
     KEY `idx_menu_target` (`target`),
@@ -186,11 +150,7 @@ CREATE TABLE IF NOT EXISTS `menu` (
     KEY `idx_menu_lft` (`lft`),
     KEY `idx_menu_rgt` (`rgt`),
     KEY `idx_menu_item` (`root_id`,`lft`,`rgt`),
-    KEY `idx_menu_system` (`is_system`),
-    KEY `idx_menu_created` (`created`),
-    KEY `idx_menu_creator` (`creator`),
-    KEY `idx_menu_modified` (`modified`),
-    KEY `idx_menu_modifier` (`modifier`)
+    KEY `idx_menu_system` (`is_system`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -204,17 +164,9 @@ CREATE TABLE IF NOT EXISTS `menu_root` (
     `id` VARCHAR(100) NOT NULL,
     `name` VARCHAR(100) NOT NULL,
     `is_system` BOOLEAN NOT NULL DEFAULT '0',
-    `created` DATETIME NOT NULL,
-    `creator` INTEGER(11) DEFAULT NULL,
-    `modified` DATETIME NOT NULL,
-    `modifier` INTEGER(11) DEFAULT NULL,
     PRIMARY KEY (`id`),
     KEY `idx_menu_root_name` (`name`),
-    KEY `idx_menu_root_system` (`is_system`),
-    KEY `idx_menu_root_created` (`created`),
-    KEY `idx_menu_root_creator` (`creator`),
-    KEY `idx_menu_root_modified` (`modified`),
-    KEY `idx_menu_root_modifier` (`modifier`)
+    KEY `idx_menu_root_system` (`is_system`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -232,21 +184,13 @@ CREATE TABLE IF NOT EXISTS `metadata` (
     `actions` TEXT DEFAULT NULL,
     `is_required` BOOLEAN NOT NULL DEFAULT '0',
     `is_unique` BOOLEAN NOT NULL DEFAULT '0',
-    `created` DATETIME NOT NULL,
-    `creator` INTEGER(11) DEFAULT NULL,
-    `modified` DATETIME NOT NULL,
-    `modifier` INTEGER(11) DEFAULT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `uni_metadata_attribute` (`entity_id`,`attribute_id`),
     KEY `idx_metadata_entity` (`entity_id`),
     KEY `idx_metadata_attribute` (`attribute_id`),
     KEY `idx_metadata_sort` (`sort_order`),
     KEY `idx_metadata_required` (`is_required`),
-    KEY `idx_metadata_unique` (`is_unique`),
-    KEY `idx_metadata_created` (`created`),
-    KEY `idx_metadata_creator` (`creator`),
-    KEY `idx_metadata_modified` (`modified`),
-    KEY `idx_metadata_modifier` (`modifier`)
+    KEY `idx_metadata_unique` (`is_unique`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -261,18 +205,10 @@ CREATE TABLE IF NOT EXISTS `rewrite` (
     `target` VARCHAR(255) NOT NULL,
     `is_redirect` BOOLEAN NOT NULL DEFAULT '0',
     `is_system` BOOLEAN NOT NULL DEFAULT '0',
-    `created` DATETIME NOT NULL,
-    `creator` INTEGER(11) DEFAULT NULL,
-    `modified` DATETIME NOT NULL,
-    `modifier` INTEGER(11) DEFAULT NULL,
     PRIMARY KEY (`id`),
     KEY `idx_rewrite_target` (`target`),
     KEY `idx_rewrite_redirect` (`is_redirect`),
-    KEY `idx_rewrite_system` (`is_system`),
-    KEY `idx_rewrite_created` (`created`),
-    KEY `idx_rewrite_creator` (`creator`),
-    KEY `idx_rewrite_modified` (`modified`),
-    KEY `idx_rewrite_modifier` (`modifier`)
+    KEY `idx_rewrite_system` (`is_system`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -288,27 +224,19 @@ CREATE TABLE IF NOT EXISTS `role` (
     `privilege` TEXT DEFAULT NULL,
     `is_active` BOOLEAN NOT NULL DEFAULT '0',
     `is_system` BOOLEAN NOT NULL DEFAULT '0',
-    `created` DATETIME NOT NULL,
-    `creator` INTEGER(11) DEFAULT NULL,
-    `modified` DATETIME NOT NULL,
-    `modifier` INTEGER(11) DEFAULT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `uni_role_name` (`name`),
     KEY `idx_role_active` (`is_active`),
-    KEY `idx_role_system` (`is_system`),
-    KEY `idx_role_created` (`created`),
-    KEY `idx_role_creator` (`creator`),
-    KEY `idx_role_modified` (`modified`),
-    KEY `idx_role_modifier` (`modifier`)
+    KEY `idx_role_system` (`is_system`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 --
 -- Data for table `role`
 --
 
-INSERT INTO `role` (`id`, `name`, `privilege`, `is_active`, `is_system`, `created`, `creator`, `modified`, `modifier`) VALUES
-(0, 'Anonymous', NULL, '1', '1', NOW(), NULL, NOW(), NULL),
-(1, 'Administrator', '["all"]', '1', '1', NOW(), NULL, NOW(), NULL);
+INSERT INTO `role` (`id`, `name`, `privilege`, `is_active`, `is_system`) VALUES
+(0, 'Anonymous', NULL, '1', '1'),
+(1, 'Administrator', '["all"]', '1', '1');
 
 -- --------------------------------------------------------
 
@@ -322,18 +250,10 @@ CREATE TABLE IF NOT EXISTS `search` (
     `entity_id` VARCHAR(100) NOT NULL,
     `content_id` VARCHAR(255) NOT NULL,
     `content` TEXT NOT NULL,
-    `created` DATETIME NOT NULL,
-    `creator` INTEGER(11) DEFAULT NULL,
-    `modified` DATETIME NOT NULL,
-    `modifier` INTEGER(11) DEFAULT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `uni_search_content` (`entity_id`,`content_id`),
     KEY `idx_search_entity` (`entity_id`),
-    KEY `idx_search_content` (`content_id`),
-    KEY `idx_search_created` (`created`),
-    KEY `idx_search_creator` (`creator`),
-    KEY `idx_search_modified` (`modified`),
-    KEY `idx_search_modifier` (`modifier`)
+    KEY `idx_search_content` (`content_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -342,16 +262,7 @@ CREATE TABLE IF NOT EXISTS `search` (
 -- Constraints for table `account`
 --
 ALTER TABLE `account`
-    ADD CONSTRAINT `con_account_role` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`),
-    ADD CONSTRAINT `con_account_creator` FOREIGN KEY (`creator`) REFERENCES `account` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
-    ADD CONSTRAINT `con_account_modifier` FOREIGN KEY (`modifier`) REFERENCES `account` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
---
--- Constraints for table `attribute`
---
-ALTER TABLE `attribute`
-    ADD CONSTRAINT `con_attribute_creator` FOREIGN KEY (`creator`) REFERENCES `account` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
-    ADD CONSTRAINT `con_attribute_modifier` FOREIGN KEY (`modifier`) REFERENCES `account` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+    ADD CONSTRAINT `con_account_role` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`);
 
 --
 -- Constraints for table `content`
@@ -367,61 +278,20 @@ ALTER TABLE `content`
 ALTER TABLE `eav`
     ADD CONSTRAINT `con_eav_entity` FOREIGN KEY (`entity_id`) REFERENCES `entity` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
     ADD CONSTRAINT `con_eav_attribute` FOREIGN KEY (`attribute_id`) REFERENCES `attribute` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    ADD CONSTRAINT `con_eav_content` FOREIGN KEY (`content_id`) REFERENCES `content` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    ADD CONSTRAINT `con_eav_creator` FOREIGN KEY (`creator`) REFERENCES `account` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
-    ADD CONSTRAINT `con_eav_modifier` FOREIGN KEY (`modifier`) REFERENCES `account` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
---
--- Constraints for table `entity`
---
-ALTER TABLE `entity`
-    ADD CONSTRAINT `con_entity_creator` FOREIGN KEY (`creator`) REFERENCES `account` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
-    ADD CONSTRAINT `con_entity_modifier` FOREIGN KEY (`modifier`) REFERENCES `account` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+    ADD CONSTRAINT `con_eav_content` FOREIGN KEY (`content_id`) REFERENCES `content` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `menu`
 --
 ALTER TABLE `menu`
-    ADD CONSTRAINT `con_menu_root` FOREIGN KEY (`root_id`) REFERENCES `menu_root` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    ADD CONSTRAINT `con_menu_creator` FOREIGN KEY (`creator`) REFERENCES `account` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
-    ADD CONSTRAINT `con_menu_modifier` FOREIGN KEY (`modifier`) REFERENCES `account` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
---
--- Constraints for table `menu_root`
---
-ALTER TABLE `menu_root`
-    ADD CONSTRAINT `con_menu_root_creator` FOREIGN KEY (`creator`) REFERENCES `account` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
-    ADD CONSTRAINT `con_menu_root_modifier` FOREIGN KEY (`modifier`) REFERENCES `account` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+    ADD CONSTRAINT `con_menu_root` FOREIGN KEY (`root_id`) REFERENCES `menu_root` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `metadata`
 --
 ALTER TABLE `metadata`
     ADD CONSTRAINT `con_metadata_entity` FOREIGN KEY (`entity_id`) REFERENCES `entity` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    ADD CONSTRAINT `con_metadata_attribute` FOREIGN KEY (`attribute_id`) REFERENCES `attribute` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    ADD CONSTRAINT `con_metadata_creator` FOREIGN KEY (`creator`) REFERENCES `account` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
-    ADD CONSTRAINT `con_metadata_modifier` FOREIGN KEY (`modifier`) REFERENCES `account` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
---
--- Constraints for table `rewrite`
---
-ALTER TABLE `rewrite`
-    ADD CONSTRAINT `con_rewrite_creator` FOREIGN KEY (`creator`) REFERENCES `account` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
-    ADD CONSTRAINT `con_rewrite_modifier` FOREIGN KEY (`modifier`) REFERENCES `account` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
---
--- Constraints for table `role`
---
-ALTER TABLE `role`
-    ADD CONSTRAINT `con_role_creator` FOREIGN KEY (`creator`) REFERENCES `account` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
-    ADD CONSTRAINT `con_role_modifier` FOREIGN KEY (`modifier`) REFERENCES `account` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
---
--- Constraints for table `search`
---
-ALTER TABLE `search`
-    ADD CONSTRAINT `con_search_creator` FOREIGN KEY (`creator`) REFERENCES `account` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
-    ADD CONSTRAINT `con_search_modifier` FOREIGN KEY (`modifier`) REFERENCES `account` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+    ADD CONSTRAINT `con_metadata_attribute` FOREIGN KEY (`attribute_id`) REFERENCES `attribute` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- --------------------------------------------------------
 
