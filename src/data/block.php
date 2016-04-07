7bef72a04e03691fa9360926481a8a7878f17512
Ayhan Akilli <info@ayhan.biz>
2016-04-07 10:50:04 +0200

    Remove unneeded comments

--- src/data/block.php
+++ src/data/block.php
@@ -1,43 +1,38 @@
 <?php
 return [
-    // entity
+    'container' => [
+        'id' => 'container',
+        'name' => 'Container',
+        'callback' => 'akilli\block_container',
+    ],
     'entity' => [
         'id' => 'entity',
         'name' => 'Entity',
         'callback' => 'akilli\block_entity',
+    ],
+    'menu' => [
+        'id' => 'menu',
+        'name' => 'Menu',
+        'callback' => 'akilli\block_menu',
+    ],
+    'message' => [
+        'id' => 'message',
+        'name' => 'Message',
+        'callback' => 'akilli\block_message',
     ],
     'pager' => [
         'id' => 'pager',
         'name' => 'Pager',
         'callback' => 'akilli\block_pager',
     ],
-    // nestedset
-    'menu' => [
-        'id' => 'menu',
-        'name' => 'Menu',
-        'callback' => 'akilli\block_menu',
-    ],
-    // session
-    'message' => [
-        'id' => 'message',
-        'name' => 'Message',
-        'callback' => 'akilli\block_message',
-    ],
-    // toolbar
-    'toolbar' => [
-        'id' => 'toolbar',
-        'name' => 'Toolbar',
-        'callback' => 'akilli\block_toolbar',
-    ],
-    // view
     'template' => [
         'id' => 'template',
         'name' => 'Template',
         'callback' => 'akilli\block_template',
     ],
-    'container' => [
-        'id' => 'container',
-        'name' => 'Container',
-        'callback' => 'akilli\block_container',
+    'toolbar' => [
+        'id' => 'toolbar',
+        'name' => 'Toolbar',
+        'callback' => 'akilli\block_toolbar',
     ],
 ];
