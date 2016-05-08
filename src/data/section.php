46bdc0838a3bcf0fc75236c317d24e616aa6118d
Ayhan Akilli <info@ayhan.biz>
2016-05-08 15:43:27 +0200

    Remove currently unused section id field

--- src/data/section.php
+++ src/data/section.php
@@ -1,37 +1,30 @@
 <?php
 return [
     'container' => [
-        'id' => 'container',
         'name' => 'Container',
         'callback' => 'qnd\section_container',
     ],
     'entity' => [
-        'id' => 'entity',
         'name' => 'Entity',
         'callback' => 'qnd\section_entity',
     ],
     'menu' => [
-        'id' => 'menu',
         'name' => 'Menu',
         'callback' => 'qnd\section_menu',
     ],
     'message' => [
-        'id' => 'message',
         'name' => 'Message',
         'callback' => 'qnd\section_message',
     ],
     'pager' => [
-        'id' => 'pager',
         'name' => 'Pager',
         'callback' => 'qnd\section_pager',
     ],
     'template' => [
-        'id' => 'template',
         'name' => 'Template',
         'callback' => 'qnd\section_template',
     ],
     'toolbar' => [
-        'id' => 'toolbar',
         'name' => 'Toolbar',
         'callback' => 'qnd\section_toolbar',
     ],
