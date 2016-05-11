f09ba7699f3feb12451b7bd72e6cab1a811c220e
Ayhan Akilli <info@ayhan.biz>
2016-05-12 01:09:25 +0200

    Continue EAV refactoring

--- src/eav.php
+++ src/eav.php
@@ -142,10 +142,13 @@ function eav_create(array & $item): bool
     $item['entity_id'] = $meta['id'];
     $cols = cols($contentAttrs, $item);
 
-    $stmt = db()->prepare(
-        'INSERT INTO ' . $meta['table']
-        . ' (' . implode(', ', $cols['col']) . ') VALUES (' . implode(', ', $cols['param']) . ')'
-    );
+    $stmt = db()->prepare('
+        INSERT INTO 
+            ' . $meta['table'] . ' 
+            (' . implode(', ', $cols['col']) . ') 
+        VALUES 
+            (' . implode(', ', $cols['param']) . ')
+    ');
 
     foreach ($cols['param'] as $code => $param) {
         $stmt->bindValue($param, $item[$code], db_type($attrs[$code], $item[$code]));
@@ -159,22 +162,23 @@ function eav_create(array & $item): bool
     }
 
     // Insert values
-    $stmt = db()->prepare(
-        'INSERT INTO eav
-         (entity_id, attribute_id, content_id, value) 
+    $stmt = db()->prepare('
+        INSERT INTO 
+            eav
+            (entity_id, attribute_id, content_id, value) 
          VALUES 
-         (:__entity_id__, :__attribute_id__, :__content_id__, :__value__)'
-    );
+            (:entity_id, :attribute_id, :content_id, :value)
+    ');
 
     foreach ($valueAttrs as $code => $attr) {
         if (!array_key_exists($code, $item)) {
             continue;
         }
 
-        $stmt->bindValue(':__entity_id__', $item['entity_id'], db_type($attrs['entity_id'], $item['entity_id']));
-        $stmt->bindValue(':__attribute_id__', $attr['id'], db_type($attr, $attr['id']));
-        $stmt->bindValue(':__content_id__', $item['id'], db_type($attrs['id'], $item['id']));
-        $stmt->bindValue(':__value__', $item[$code], db_type($attr, $item[$code]));
+        $stmt->bindValue(':entity_id', $item['entity_id'], db_type($attrs['entity_id'], $item['entity_id']));
+        $stmt->bindValue(':attribute_id', $attr['id'], db_type($attr, $attr['id']));
+        $stmt->bindValue(':content_id', $item['id'], db_type($attrs['id'], $item['id']));
+        $stmt->bindValue(':value', $item[$code], db_type($attr, $item[$code]));
         $stmt->execute();
     }
 
@@ -201,22 +205,18 @@ function eav_save(array & $item): bool
     $attrs = $meta['attributes'];
     $contentAttrs = $contentMeta['attributes'];
     $valueAttrs = array_diff_key($attrs, $contentAttrs);
-    $valueModel = meta_skeleton('eav');
-
-    if ($valueAttrs) {
-        $values = entity_load('eav', ['content_id' => $item['_old']['id']], 'attribute_id');
-    } else {
-        $values = [];
-    }
-
+    $values = $valueAttrs ? entity_load('eav', ['content_id' => $item['_old']['id']], 'attribute_id') : [];
     $item['entity_id'] = $meta['id'];
     $cols = cols($contentAttrs, $item);
 
-    $stmt = db()->prepare(
-        'UPDATE ' . $meta['table']
-        . ' SET ' . implode(', ', $cols['set'])
-        . ' WHERE ' . $attrs['id']['column'] . '  = :id'
-    );
+    $stmt = db()->prepare('
+        UPDATE 
+            content 
+        SET 
+            ' . implode(', ', $cols['set']) . ' 
+        WHERE 
+            ' . $attrs['id']['column'] . '  = :id
+    ');
 
     foreach ($cols['param'] as $code => $param) {
         $stmt->bindValue($param, $item[$code], db_type($attrs[$code], $item[$code]));
@@ -226,31 +226,37 @@ function eav_save(array & $item): bool
     $stmt->execute();
 
     // Save values
-    if ($valueAttrs) {
-        $save = [];
-        $i = 0;
+    $insert = db()->prepare('
+        INSERT INTO 
+            eav
+            (entity_id, attribute_id, content_id, value) 
+         VALUES 
+            (:entity_id, :attribute_id, :content_id, :value)
+    ');
+    $update = db()->prepare('
+        UPDATE 
+            eav
+        SET
+            value = :value
+        WHERE
+            id = :id
+    ');
 
-        foreach ($valueAttrs as $code => $attr) {
-            if (!array_key_exists($code, $item)) {
-                continue;
-            }
-
-            $valueItem = [
-                'entity_id' => $item['entity_id'],
-                'attribute_id' => $attr['id'],
-                'content_id' => $item['id'],
-                'value' => $item[$code]
-            ];
-
-            if (!empty($values[$code])) {
-                $save[$values[$code]['id']] = array_replace($values[$code], $valueItem);
-            } else {
-                $save[--$i] = array_replace($valueModel, $valueItem);
-            }
+    foreach ($valueAttrs as $code => $attr) {
+        if (!array_key_exists($code, $item)) {
+            continue;
         }
 
-        if (!entity_save('eav', $save)) {
-            throw new RuntimeException(_('Data could not be saved'));
+        if (!empty($values[$code])) {
+            $update->bindValue(':id', $values[$code]['id'], PDO::PARAM_INT);
+            $update->bindValue(':value', $item[$code], db_type($attr, $item[$code]));
+            $update->execute();
+        } else {
+            $insert->bindValue(':entity_id', $item['entity_id'], db_type($attrs['entity_id'], $item['entity_id']));
+            $insert->bindValue(':attribute_id', $attr['id'], db_type($attr, $attr['id']));
+            $insert->bindValue(':content_id', $item['id'], db_type($attrs['id'], $item['id']));
+            $insert->bindValue(':value', $item[$code], db_type($attr, $item[$code]));
+            $insert->execute();
         }
     }
 
