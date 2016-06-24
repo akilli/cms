e9d95a79a8669db2f500dc1bea98ba30874b6d48
Ayhan Akilli <info@ayhan.biz>
2016-06-24 14:00:21 +0200

    Add unzip() and translations

---
+++ src/zip.php
@@ -0,0 +1,43 @@
+<?php
+namespace qnd;
+
+use InvalidArgumentException;
+use RuntimeException;
+use ZipArchive;
+
+/**
+ * Unzip file
+ *
+ * @param string $file
+ * @param string $path
+ *
+ * @return bool
+ *
+ * @throws RuntimeException
+ */
+function unzip(string $file, string $path): bool
+{
+    if (!is_file($file)) {
+        throw new InvalidArgumentException(_('File %s does not exist', $file));
+    }
+
+    if (!file_writable($path)) {
+        throw new RuntimeException(_('Path %s is not writable', $path));
+    }
+
+    if (file_exists($path)) {
+        throw new RuntimeException(_('Path %s already exists', $path));
+    }
+
+    $zip = new ZipArchive();
+
+    if (!$zip->open($file)) {
+        throw new RuntimeException(_('Could not open ZIP file %s', $file));
+    }
+
+    if (!$zip->extractTo($path)) {
+        throw new RuntimeException(_('Could not extract ZIP file to %s', $path));
+    }
+
+    return $zip->close();
+}
