2eb4a8005282652c9bf8bf264c3532a245ce4904
Ayhan Akilli <info@ayhan.biz>
2016-07-05 09:35:51 +0200

    Clean up image()

--- src/media.php
+++ src/media.php
@@ -44,67 +44,64 @@ function image(string $file, array $opts): string
         }
     }
 
-    // Asset
-    $assetId = $media['id'] . '/' . $opts['width'] . '-' . $opts['height'] . ($opts['crop'] ? '-crop' : '') . '.' . $media['ext'];
-    $assetPath = project_path('asset', 'media/' . $assetId);
+    // Cache
+    $id = $media['id'] . '/' . $opts['width'] . '-' . $opts['height'] . ($opts['crop'] ? '-crop' : '') . '.' . $media['ext'];
+    $path = project_path('asset', 'media/' . $id);
 
-    // Generate asset file
-    if (!file_exists($assetPath) || $media['modified'] >= filemtime($assetPath)) {
-        if ($info[0] === $opts['width'] && $info[1] === $opts['height']) {
-            file_copy($media['path'], $assetPath);
-        } else {
-            // Callbacks
-            if ($info[2] === IMAGETYPE_JPEG) {
-                $create = 'imagecreatefromjpeg';
-                $output = 'imagejpeg';
-            } elseif ($info[2] === IMAGETYPE_PNG) {
-                $create = 'imagecreatefrompng';
-                $output = 'imagepng';
-                $opts['quality'] = round(($opts['quality'] / 100) * 10);
-
-                if ($opts['quality'] < 1) {
-                    $opts['quality'] = 1;
-                } elseif ($opts['quality'] > 10) {
-                    $opts['quality'] = 10;
-                }
-
-                $opts['quality'] = 10 - $opts['quality'];
-            } elseif ($info[2] === IMAGETYPE_GIF) {
-                $create = 'imagecreatefromgif';
-                $output = 'imagegif';
-            } elseif ($media['ext'] = 'webp') {
-                $create = 'imagecreatefromwebp';
-                $output = 'imagewebp';
-            }
-
-            if (!empty($create) && !empty($output)) {
-                // Make asset directory
-                file_dir(dirname($assetPath));
-
-                // Resource
-                $source = $create($media['path']);
-                $image = imagecreatetruecolor($opts['width'], $opts['height']);
-
-                // Transparency
-                $alpha = imagecolorallocatealpha($image, 0, 0, 0, 127);
-                imagecolortransparent($image, $alpha);
-                imagealphablending($image, false);
-                imagesavealpha($image, true);
-
-                // Output
-                imagecopyresampled($image, $source, 0, 0, $x, $y, $opts['width'], $opts['height'], $width, $height);
-                $umask = umask(0);
-                $output($image, $assetPath, $opts['quality']);
-                umask($umask);
-
-                // Destroy
-                imagedestroy($source);
-                imagedestroy($image);
-            }
-        }
+    if (file_exists($path) && $media['modified'] < filemtime($path)) {
+        return url_asset('media/' . $id);
     }
 
-    return file_exists($assetPath) ? url_asset('media/' . $assetId) : url_media($media['id']);
+    if ($info[0] === $opts['width'] && $info[1] === $opts['height']) {
+        return file_copy($media['path'], $path) ? url_asset('media/' . $id) : url_media($media['id']);
+    }
+
+    // Callbacks
+    if ($info[2] === IMAGETYPE_JPEG) {
+        $create = 'imagecreatefromjpeg';
+        $output = 'imagejpeg';
+    } elseif ($info[2] === IMAGETYPE_PNG) {
+        $create = 'imagecreatefrompng';
+        $output = 'imagepng';
+        $opts['quality'] = round(($opts['quality'] / 100) * 10);
+
+        if ($opts['quality'] < 1) {
+            $opts['quality'] = 1;
+        } elseif ($opts['quality'] > 10) {
+            $opts['quality'] = 10;
+        }
+
+        $opts['quality'] = 10 - $opts['quality'];
+    } elseif ($info[2] === IMAGETYPE_GIF) {
+        $create = 'imagecreatefromgif';
+        $output = 'imagegif';
+    } elseif ($media['ext'] === 'webp') {
+        $create = 'imagecreatefromwebp';
+        $output = 'imagewebp';
+    } else {
+        return url_media($media['id']);
+    }
+
+    // Make asset directory
+    file_dir(dirname($path));
+    // Resource
+    $source = $create($media['path']);
+    $image = imagecreatetruecolor($opts['width'], $opts['height']);
+    // Transparency
+    $alpha = imagecolorallocatealpha($image, 0, 0, 0, 127);
+    imagecolortransparent($image, $alpha);
+    imagealphablending($image, false);
+    imagesavealpha($image, true);
+    // Output
+    imagecopyresampled($image, $source, 0, 0, $x, $y, $opts['width'], $opts['height'], $width, $height);
+    $umask = umask(0);
+    $output($image, $path, $opts['quality']);
+    umask($umask);
+    // Destroy
+    imagedestroy($source);
+    imagedestroy($image);
+
+    return is_file($path) ? url_asset('media/' . $id) : url_media($media['id']);
 }
 
 /**
