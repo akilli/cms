cf818ca65db065b5e31e4c92a7bf806840e387eb
Ayhan Akilli <info@ayhan.biz>
2016-07-10 14:54:36 +0200

    Implement checkall toggle

--- public/theme/base/js/theme.js
+++ public/theme/base/js/theme.js
@@ -4,6 +4,15 @@
 
     $(function()
     {
+        // Rich Text Editor
         $('[data-type=rte]').ckeditor();
+
+        // Toggle
+        $('[data-toggle]').on('change', function()
+        {
+            var $this = $(this);
+            $('[data-toggle-id=' + $this.data('toggle') + ']').prop('checked', $this.prop('checked'));
+
+        });
     });
 })(jQuery, window, document);
