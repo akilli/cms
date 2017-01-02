030708c4c911d6d2180cf75d15474457a9cde7e1
Ayhan Akilli <info@ayhan.biz>
2017-01-02 09:14:35 +0100

    Split & rename SQL dumps

--- src/sql/app.my.sql
+++ src/sql/app.my.sql
@@ -1,12 +1,6 @@
 SET FOREIGN_KEY_CHECKS=0;
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
 
 START TRANSACTION;
-
--- --------------------------------------------------------
--- Tables
--- --------------------------------------------------------
 
 DROP TABLE IF EXISTS attr;
 CREATE TABLE IF NOT EXISTS attr (
@@ -203,45 +197,6 @@ CREATE TABLE IF NOT EXISTS user (
     CONSTRAINT con_user_role FOREIGN KEY (role_id) REFERENCES role (id),
     CONSTRAINT con_user_project FOREIGN KEY (project_id) REFERENCES project (id) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
--- Data
--- --------------------------------------------------------
-
-INSERT INTO entity (id, name, actions, system, project_id) VALUES
-('page', 'Page', '["admin", "create", "delete", "edit", "index", "view"]', '1', 'base');
-
-INSERT INTO menu (id, uid, name, system, project_id) VALUES
-(1, 'toolbar', 'Toolbar', '1', 'base');
-
-INSERT INTO node (id, name, target, root_id, lft, rgt, level, project_id) VALUES
-(1, 'Homepage', '/', 1, 1, 2, 1, 'base'),
-(2, 'Dashboard', '/user/dashboard', 1, 3, 4, 1, 'base'),
-(3, 'Profile', '/user/profile', 1, 5, 6, 1, 'base'),
-(4, 'Logout', '/user/logout', 1, 7, 8, 1, 'base'),
-(5, 'Content', '', 1, 9, 12, 1, 'base'),
-(6, 'Page', '/page/admin', 1, 10, 11, 2, 'base'),
-(7, 'Structure', '', 1, 13, 22, 1, 'base'),
-(8, 'Menu', '/menu/admin', 1, 14, 15, 2, 'base'),
-(9, 'Node', '/node/admin', 1, 16, 17, 2, 'base'),
-(10, 'Entity', '/entity/admin', 1, 18, 19, 2, 'base'),
-(11, 'Attribute', '/attr/admin', 1, 20, 21, 2, 'base'),
-(12, 'System', '', 1, 23, 32, 1, 'base'),
-(13, 'Project', '/project/admin', 1, 24, 25, 2, 'base'),
-(14, 'User', '/user/admin', 1, 26, 27, 2, 'base'),
-(15, 'Role', '/role/admin', 1, 28, 29, 2, 'base'),
-(16, 'URL', '/url/admin', 1, 30, 31, 2, 'base');
-
-INSERT INTO project (id, name, host, active, system) VALUES
-('base', 'BASE', NULL, '1', '1');
-
-INSERT INTO role (id, name, privilege, active, system, project_id) VALUES
-(1, 'admin', '[]', '1', '1', 'base');
-
-INSERT INTO user (id, name, username, password, role_id, active, system, project_id) VALUES
-(1, 'Admin', 'admin', '$2y$10$9wnkOfY1qLvz0sRXG5G.d.rf2NhCU8a9m.XrLYIgeQA.SioSWwtsW', 1, '1', '1', 'base');
-
--- --------------------------------------------------------
 
 COMMIT;
 
---
+++ src/sql/data.sql
@@ -0,0 +1,42 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+START TRANSACTION;
+
+INSERT INTO entity (id, name, actions, system, project_id) VALUES
+('page', 'Page', '["admin", "create", "delete", "edit", "index", "view"]', '1', 'base');
+
+INSERT INTO menu (id, uid, name, system, project_id) VALUES
+(1, 'toolbar', 'Toolbar', '1', 'base');
+
+INSERT INTO node (id, name, target, root_id, lft, rgt, level, project_id) VALUES
+(1, 'Homepage', '/', 1, 1, 2, 1, 'base'),
+(2, 'Dashboard', '/user/dashboard', 1, 3, 4, 1, 'base'),
+(3, 'Profile', '/user/profile', 1, 5, 6, 1, 'base'),
+(4, 'Logout', '/user/logout', 1, 7, 8, 1, 'base'),
+(5, 'Content', '', 1, 9, 12, 1, 'base'),
+(6, 'Page', '/page/admin', 1, 10, 11, 2, 'base'),
+(7, 'Structure', '', 1, 13, 22, 1, 'base'),
+(8, 'Menu', '/menu/admin', 1, 14, 15, 2, 'base'),
+(9, 'Node', '/node/admin', 1, 16, 17, 2, 'base'),
+(10, 'Entity', '/entity/admin', 1, 18, 19, 2, 'base'),
+(11, 'Attribute', '/attr/admin', 1, 20, 21, 2, 'base'),
+(12, 'System', '', 1, 23, 32, 1, 'base'),
+(13, 'Project', '/project/admin', 1, 24, 25, 2, 'base'),
+(14, 'User', '/user/admin', 1, 26, 27, 2, 'base'),
+(15, 'Role', '/role/admin', 1, 28, 29, 2, 'base'),
+(16, 'URL', '/url/admin', 1, 30, 31, 2, 'base');
+
+INSERT INTO project (id, name, host, active, system) VALUES
+('base', 'BASE', NULL, '1', '1');
+
+INSERT INTO role (id, name, privilege, active, system, project_id) VALUES
+(1, 'admin', '[]', '1', '1', 'base');
+
+INSERT INTO user (id, name, username, password, role_id, active, system, project_id) VALUES
+(1, 'Admin', 'admin', '$2y$10$9wnkOfY1qLvz0sRXG5G.d.rf2NhCU8a9m.XrLYIgeQA.SioSWwtsW', 1, '1', '1', 'base');
+
+-- --------------------------------------------------------
+
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS=1;
