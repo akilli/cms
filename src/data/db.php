02a02672f7ffb221b612858afab3a2b17b672264
Ayhan Akilli <info@ayhan.biz>
2017-01-04 00:34:39 +0100

    db data

--- src/data/db.php
+++ src/data/db.php
@@ -1,8 +1,11 @@
 <?php
 return [
-    'dsn' => 'mysql:host=mysql;dbname=qnd;charset=utf8',
-    'user' => 'root',
-    'password' => 'root',
+    'driver' => 'pgsql',
+    'host' => 'postgres',
+    'db' => 'qnd',
+    'user' => 'postgres',
+    'password' => 'postgres',
+    'charset' => 'utf8',
     'driver_options' => [
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
