19182b5c49c93623fb39351992b00660c219712e
Ayhan Akilli <info@ayhan.biz>
2017-01-04 15:34:19 +0100

    data.sql .... reduce to one psql sequence problem

--- src/sql/data.sql
+++ src/sql/data.sql
@@ -1,11 +1,23 @@
-INSERT INTO project (id, name, host, active, system) VALUES
+INSERT INTO
+    project
+    (id, name, host, active, system)
+VALUES
     ('base', 'BASE', NULL, TRUE, TRUE);
 
-INSERT INTO role (id, name, privilege, active, system, project_id) VALUES
+INSERT INTO
+    role
+    (id, name, privilege, active, system, project_id)
+VALUES
     (1, 'admin', '["_all_"]', TRUE, TRUE, 'base');
 
-INSERT INTO account (id, name, username, password, role_id, active, system, project_id) VALUES
-    (1, 'Admin', 'admin', '$2y$10$9wnkOfY1qLvz0sRXG5G.d.rf2NhCU8a9m.XrLYIgeQA.SioSWwtsW', 1, TRUE, TRUE, 'base');
+INSERT INTO
+    account
+    (name, username, password, role_id, active, system, project_id)
+VALUES
+    ('Admin', 'admin', '$2y$10$9wnkOfY1qLvz0sRXG5G.d.rf2NhCU8a9m.XrLYIgeQA.SioSWwtsW', 1, TRUE, TRUE, 'base');
 
-INSERT INTO entity (id, name, actions, system, project_id) VALUES
+INSERT INTO
+    entity
+    (id, name, actions, system, project_id)
+VALUES
     ('page', 'Page', '["admin", "create", "delete", "edit", "index", "view"]', TRUE, 'base');
