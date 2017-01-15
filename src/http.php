9b384959812bfdfee0c52ac83a5e2acaaa081767
Ayhan Akilli <info@ayhan.biz>
2017-01-15 23:48:28 +0100

    Replace old session()

--- src/http.php
+++ src/http.php
@@ -12,28 +12,17 @@ namespace qnd;
  */
 function session(string $key, $value = null, bool $reset = false)
 {
-    static $data;
-
-    if ($data === null) {
-        // Start session if it was not started before
-        if (session_status() !== PHP_SESSION_ACTIVE) {
-            session_start();
-        }
-
-        $data = & $_SESSION;
+    if (session_status() !== PHP_SESSION_ACTIVE) {
+        session_start();
     }
 
-    // If $value is provided, set $value for $key
-    if ($value !== null) {
-        $data[$key] = $value;
+    if ($reset) {
+        unset($_SESSION[$key]);
+    } elseif ($value !== null) {
+        $_SESSION[$key] = $value;
     }
 
-    // Reset value or set $key in first call, so we have something to return
-    if (isset($data[$key]) && $reset || !isset($data[$key])) {
-        $data[$key] = null;
-    }
-
-    return $data[$key];
+    return $_SESSION[$key] ?? null;
 }
 
 /**
