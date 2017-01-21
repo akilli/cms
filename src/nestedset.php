583d1848cc266677127960ac4ffc203753b8501c
Ayhan Akilli <info@ayhan.biz>
2017-01-21 14:19:52 +0100

    Handle nestedset model in DB

--- src/nestedset.php
+++ src/nestedset.php
@@ -1,8 +1,6 @@
 <?php
 namespace qnd;
 
-use LogicException;
-
 /**
  * Load entity
  *
@@ -29,32 +27,12 @@ function nestedset_load(array $entity, array $crit = [], array $opts = []): arra
  */
 function nestedset_save(array & $item): bool
 {
-    // New node
-    if (empty($item['_old'])) {
-        $item = nestedset_position($item);
-        nestedset_prepare($item);
+    $attrs = $item['_entity']['attr'];
+    $parts = explode(':', $item['pos']);
+    $item['root_id'] = cast($attrs['root_id'], $parts[0]);
+    $item['lft'] = ($item['mode'] === 'child' ? -1 : 1) * cast($attrs['lft'], $parts[1]);
 
-        return flat_save($item);
-    }
-
-    // Update all attributes that are not involved with the tree
-    $temp = $item;
-    unset($temp['root_id'], $temp['lft'], $temp['rgt'], $temp['level']);
-    flat_save($temp);
-
-    // No change in position, so nothing to do anymore
-    if ($item['pos'] === $item['_old']['pos']) {
-        return true;
-    }
-
-    // Handle tree changes
-    $item = nestedset_position($item);
-    nestedset_move($item);
-    nestedset_remove($item);
-    nestedset_prepare($item);
-    nestedset_insert($item);
-
-    return true;
+    return flat_save($item);
 }
 
 /**
@@ -66,195 +44,5 @@ function nestedset_save(array & $item): bool
  */
 function nestedset_delete(array & $item): bool
 {
-    $attrs = $item['_entity']['attr'];
-
-    $stmt = db_prep(
-        'DELETE FROM %s WHERE %s = :root_id AND %s BETWEEN :lft AND :rgt',
-        $item['_entity']['tab'],
-        $attrs['root_id']['col'],
-        $attrs['lft']['col']
-    );
-    $stmt->bindValue(':root_id', $item['_old']['root_id'], $attrs['root_id']['pdo']);
-    $stmt->bindValue(':lft', $item['_old']['lft'], $attrs['lft']['pdo']);
-    $stmt->bindValue(':rgt', $item['_old']['rgt'], $attrs['lft']['pdo']);
-    $stmt->execute();
-
-    // Close gap in old tree
-    nestedset_remove($item);
-
-    return true;
-}
-
-/**
- * Calculate node postion
- *
- * @param array $item
- *
- * @return array
- *
- * @throws LogicException
- */
-function nestedset_position(array $item): array
-{
-    $attrs = $item['_entity']['attr'];
-    $old = $item['_old'] ?? null;
-    $range = $old ? $old['rgt'] - $old['lft'] + 1 : 2;
-    $parts = explode(':', $item['pos']);
-    $item['root_id'] = cast($attrs['root_id'], $parts[0]);
-    $bLft = (int) $parts[1];
-
-    if (!$bLft || !($base = one($item['_entity']['uid'], ['root_id' => $item['root_id'], 'lft' => $bLft]))) {
-        // Append
-        $stmt = db_prep(
-            'SELECT COALESCE(MAX(%s), 0) + 1 FROM %s WHERE %s = :root_id',
-            $attrs['rgt']['col'],
-            $item['_entity']['tab'],
-            $attrs['root_id']['col']
-        );
-        $stmt->bindValue(':root_id', $item['root_id'], $attrs['root_id']['pdo']);
-        $stmt->execute();
-
-        $item['lft'] = (int) $stmt->fetchColumn();
-        $item['level'] = 1;
-    } elseif ($old && $item['root_id'] === $old['root_id'] && $old['lft'] < $base['lft'] && $old['rgt'] > $base['rgt']) {
-        // Recursion
-        throw new LogicException(_('Node can not be child of itself'));
-    } elseif ($item['mode'] === 'child') {
-        // Add child
-        $item['lft'] = $base['rgt'];
-        $item['level'] = $base['level'] + 1;
-    } else {
-        // Add before
-        $item['lft'] = $base['lft'];
-        $item['level'] = $base['level'];
-    }
-
-    if ($old && $item['root_id'] === $old['root_id'] && $item['lft'] > $old['lft']) {
-        $item['lft'] -= $range;
-    }
-
-    $item['rgt'] = $item['lft'] + $range - 1;
-
-    return $item;
-}
-
-/**
- * Move all affected nodes from old tree and update their positions for the new tree without adding them yet
- *
- * @param array $item
- *
- * @return void
- */
-function nestedset_move(array $item): void
-{
-    $attrs = $item['_entity']['attr'];
-
-    $stmt = db_prep(
-        'UPDATE %1$s SET %2$s = :root_id, %3$s = -1 * (%3$s + :lft_diff), %4$s = -1 * (%4$s + :rgt_diff) WHERE %2$s = :old_root_id AND %3$s BETWEEN :lft AND :rgt',
-        $item['_entity']['tab'],
-        $attrs['root_id']['col'],
-        $attrs['lft']['col'],
-        $attrs['rgt']['col']
-    );
-    $stmt->bindValue(':root_id', $item['root_id'], $attrs['root_id']['pdo']);
-    $stmt->bindValue(':old_root_id', $item['_old']['root_id'], $attrs['root_id']['pdo']);
-    $stmt->bindValue(':lft', $item['_old']['lft'], $attrs['lft']['pdo']);
-    $stmt->bindValue(':lft_diff', $item['lft'] - $item['_old']['lft'], $attrs['lft']['pdo']);
-    $stmt->bindValue(':rgt', $item['_old']['rgt'], $attrs['rgt']['pdo']);
-    $stmt->bindValue(':rgt_diff', $item['lft'] - $item['_old']['lft'], $attrs['rgt']['pdo']);
-    $stmt->execute();
-}
-
-/**
- * Close gap in old tree
- *
- * @param array $item
- *
- * @return void
- */
-function nestedset_remove(array $item): void
-{
-    $attrs = $item['_entity']['attr'];
-    $range = $item['_old']['rgt'] - $item['_old']['lft'] + 1;
-
-    $stmt = db_prep(
-        'UPDATE %1$s SET %3$s = %3$s - :range WHERE %2$s = :root_id AND %3$s > :rgt',
-        $item['_entity']['tab'],
-        $attrs['root_id']['col'],
-        $attrs['lft']['col']
-    );
-    $stmt->bindValue(':root_id', $item['_old']['root_id'], $attrs['root_id']['pdo']);
-    $stmt->bindValue(':rgt', $item['_old']['rgt'], $attrs['lft']['pdo']);
-    $stmt->bindValue(':range', $range, $attrs['lft']['pdo']);
-    $stmt->execute();
-
-    $stmt = db_prep(
-        'UPDATE %1$s SET %3$s = %3$s - :range WHERE %2$s = :root_id AND %3$s > :rgt',
-        $item['_entity']['tab'],
-        $attrs['root_id']['col'],
-        $attrs['rgt']['col']
-    );
-    $stmt->bindValue(':root_id', $item['_old']['root_id'], $attrs['root_id']['pdo']);
-    $stmt->bindValue(':rgt', $item['_old']['rgt'], $attrs['rgt']['pdo']);
-    $stmt->bindValue(':range', $range, $attrs['rgt']['pdo']);
-    $stmt->execute();
-}
-
-/**
- * Make space in new tree
- *
- * @param array $item
- *
- * @return void
- */
-function nestedset_prepare(array $item): void
-{
-    $attrs = $item['_entity']['attr'];
-    $range = $item['rgt'] - $item['lft'] + 1;
-
-    $stmt = db_prep(
-        'UPDATE %1$s SET %3$s = %3$s + :range WHERE %2$s = :root_id AND %3$s >= :lft',
-        $item['_entity']['tab'],
-        $attrs['root_id']['col'],
-        $attrs['lft']['col']
-    );
-    $stmt->bindValue(':root_id', $item['root_id'], $attrs['root_id']['pdo']);
-    $stmt->bindValue(':lft', $item['lft'], $attrs['lft']['pdo']);
-    $stmt->bindValue(':range', $range, $attrs['lft']['pdo']);
-    $stmt->execute();
-
-    $stmt = db_prep(
-        'UPDATE %1$s SET %3$s = %3$s + :range WHERE %2$s = :root_id AND %3$s >= :lft',
-        $item['_entity']['tab'],
-        $attrs['root_id']['col'],
-        $attrs['rgt']['col']
-    );
-    $stmt->bindValue(':root_id', $item['root_id'], $attrs['root_id']['pdo']);
-    $stmt->bindValue(':lft', $item['lft'], $attrs['rgt']['pdo']);
-    $stmt->bindValue(':range', $range, $attrs['rgt']['pdo']);
-    $stmt->execute();
-}
-
-/**
- * Add affected nodes to new tree
- *
- * @param array $item
- *
- * @return void
- */
-function nestedset_insert(array $item): void
-{
-    $attrs = $item['_entity']['attr'];
-
-    $stmt = db_prep(
-        'UPDATE %1$s SET %3$s = -1 * %3$s, %4$s = -1 * %4$s, %5$s = %5$s + :level WHERE %2$s = :root_id AND %3$s < 0',
-        $item['_entity']['tab'],
-        $attrs['root_id']['col'],
-        $attrs['lft']['col'],
-        $attrs['rgt']['col'],
-        $attrs['level']['col']
-    );
-    $stmt->bindValue(':root_id', $item['root_id'], $attrs['root_id']['pdo']);
-    $stmt->bindValue(':level', $item['level'] - $item['_old']['level'], $attrs['level']['pdo']);
-    $stmt->execute();
+    return flat_delete($item);
 }
