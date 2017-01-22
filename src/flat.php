07af1cb4db09f110a2e7a0f50e5b26e7e2a2eb0c
Ayhan Akilli <info@ayhan.biz>
2017-01-22 12:58:34 +0100

    flat: add default order

--- src/flat.php
+++ src/flat.php
@@ -13,6 +13,7 @@ namespace qnd;
 function flat_load(array $entity, array $crit = [], array $opts = []): array
 {
     $attrs = db_attr($entity['attr']);
+    $opts['order'] = $opts['mode'] === 'size' || $opts['order'] ? $opts['order'] : ['id' => 'asc'];
     $select = $opts['mode'] === 'size' ? ['COUNT(*)'] : array_column($attrs, 'col') + $opts['select'];
     $stmt = db()->prepare(
         select($select)
