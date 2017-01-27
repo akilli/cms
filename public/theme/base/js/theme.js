59484da00e2552e34b375e6663b52d911d71fba9
Ayhan Akilli <info@ayhan.biz>
2017-01-27 21:43:27 +0100

    Clean up

--- public/theme/base/js/theme.js
+++ public/theme/base/js/theme.js
@@ -4,21 +4,23 @@
     document.addEventListener('DOMContentLoaded', function()
     {
         // Rich Text Editor
-        var rte = document.querySelectorAll('[data-type=rte]');
+        const rte = document.querySelectorAll('[data-type=rte]');
 
-        for (var i = 0; i < rte.length; ++i) {
+        for (let i = 0; i < rte.length; ++i) {
             new RTE(rte[i]);
         }
 
         // Toggle
-        var toggle = document.querySelectorAll('[data-toggle]');
+        const toggles = document.querySelectorAll('[data-toggle]');
 
-        for (var i = 0; i < toggle.length; ++i) {
-            toggle[i].addEventListener('change', function () {
-                var cb = document.querySelectorAll('input[type=checkbox][data-toggle-id=' + this.getAttribute('data-toggle') + ']');
+        for (let i = 0; i < toggles.length; ++i) {
+            let toggle = toggles[i];
 
-                for (var i = 0; i < cb.length; ++i) {
-                    cb[i].click();
+            toggle.addEventListener('change', function () {
+                const cb = document.querySelectorAll('input[type=checkbox][data-toggle-id=' + this.getAttribute('data-toggle') + ']');
+
+                for (let i = 0; i < cb.length; ++i) {
+                    cb[i].checked = toggle.checked;
                 }
             });
         }
