dbca005cb4d57e023d25be239abda00001acf5db
Ayhan Akilli <info@ayhan.biz>
2017-03-17 09:37:59 +0100

    Fix #38 unwanted browser autocomplete functionality

--- public/theme/base/js/theme.js
+++ public/theme/base/js/theme.js
@@ -6,23 +6,35 @@
         // Rich Text Editor
         const rte = document.querySelectorAll('[data-type=rte]');
 
-        for (let i = 0; i < rte.length; ++i) {
+        for (let i = 0; i < rte.length; i++) {
             CKEDITOR.replace(rte[i]);
         }
 
         // Toggle
         const toggles = document.querySelectorAll('[data-toggle]');
 
-        for (let i = 0; i < toggles.length; ++i) {
+        for (let i = 0; i < toggles.length; i++) {
             let toggle = toggles[i];
 
             toggle.addEventListener('change', function () {
                 const cb = document.querySelectorAll('input[type=checkbox][data-toggle-id=' + this.getAttribute('data-toggle') + ']');
 
-                for (let i = 0; i < cb.length; ++i) {
+                for (let i = 0; i < cb.length; i++) {
                     cb[i].checked = toggle.checked;
                 }
             });
+        }
+
+        // Input password autocomplete fix
+        const inputs = document.querySelectorAll('input[type=password]');
+
+        for (let i = 0; i < inputs.length; i++) {
+            let input = inputs[i];
+
+            input.setAttribute('readonly', true);
+            input.addEventListener('focus', function () {
+                input.removeAttribute('readonly');
+            })
         }
     });
 })(document);
