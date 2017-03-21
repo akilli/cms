26f2302285ee1c8c845639a1864d7237de61d08d
Ayhan Akilli <info@ayhan.biz>
2017-03-21 18:02:57 +0100

    Export skeleton

---
+++ src/export.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types = 1);
+
+namespace qnd;
+
+use RuntimeException;
+
+/**
+ * Export project
+ *
+ * @param int $id
+ *
+ * @return string
+ *
+ * @throws RuntimeException
+ */
+function export(int $id): string
+{
+    if (!$project = one('project', ['id' => $id])) {
+        throw new RuntimeException(_('Invalid project ID %d', (string) $id));
+    }
+
+    return '';
+}
