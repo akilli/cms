2423fb8aeb053b9ffb22c2b86eba16658664b44d
Ayhan Akilli <info@ayhan.biz>
2017-03-23 01:33:12 +0100

    Fix type

--- src/entity.php
+++ src/entity.php
@@ -245,7 +245,7 @@ function delete(string $eUid, array $crit = [], array $opts = []): bool
 
     foreach (all($eUid, $crit, $opts) as $id => $item) {
         if (empty($opts['system']) && !empty($item['system'])) {
-            message(_('System items must not be deleted! Therefore skipped Id %s', $id));
+            message(_('System items must not be deleted! Therefore skipped Id %s', (string) $id));
             continue;
         }
 
