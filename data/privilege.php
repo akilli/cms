59a196cc395a3e0e6038b2566e8463a33d98731e
Ayhan Akilli <info@ayhan.biz>
2017-03-27 11:41:31 +0200

    Clean up privilege data

--- data/privilege.php
+++ data/privilege.php
@@ -1,21 +1,9 @@
 <?php
 return [
-    '_all_' => [
-        'name' => 'ALL PRIVILEGES',
-    ],
-    'account.dashboard' => [
-        'callback' => 'registered',
-    ],
-    'account.login' => [
-        'callback' => 'unregistered',
-    ],
-    'account.logout' => [
-        'callback' => 'registered',
-    ],
-    'account.password' => [
-        'callback' => 'registered',
-    ],
-    'project.switch' => [
-        'callback' => 'account_global',
-    ],
+    '_all_' => ['name' => 'ALL PRIVILEGES'],
+    'account.dashboard' => ['callback' => 'registered'],
+    'account.login' => ['callback' => 'unregistered'],
+    'account.logout' => ['callback' => 'registered'],
+    'account.password' => ['callback' => 'registered'],
+    'project.switch' => ['callback' => 'account_global'],
 ];
