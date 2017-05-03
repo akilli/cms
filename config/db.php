fe96dfd644b168a2dc3e51965a6c3a02c5231ca6
Ayhan Akilli <info@ayhan.biz>
2017-05-03 13:16:23 +0200

    Fix db config

--- config/db.php
+++ config/db.php
@@ -1,9 +1,9 @@
 <?php
 return [
-    'host' => 'postgres',
-    'db' => 'qnd',
-    'user' => 'postgres',
-    'password' => 'postgres',
+    'host' => 'data',
+    'db' => 'app',
+    'user' => 'app',
+    'password' => 'app',
     'opt' => [
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
