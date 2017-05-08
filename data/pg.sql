03e6c2a39839a22da3ace833a658c3117bcf4a9f
Ayhan Akilli <info@ayhan.biz>
2017-05-08 21:26:13 +0200

    Use explicit IDs

--- data/pg.sql
+++ data/pg.sql
@@ -205,21 +205,21 @@ CREATE INDEX ON tree (project_id);
 
 INSERT INTO
     project
-    (uid, name, active, system)
+    (id, uid, name, active, system)
 VALUES
-    ('www', 'WWW', TRUE, TRUE);
+    (1, 'www', 'WWW', TRUE, TRUE);
 
 INSERT INTO
     role
-    (name, privilege, active, system, project_id)
+    (id, name, privilege, active, system, project_id)
 VALUES
-    ('admin', '["_all_"]', TRUE, TRUE, CURRVAL('project_id_seq'));
+    (1, 'admin', '["_all_"]', TRUE, TRUE, 1);
 
 INSERT INTO
     account
-    (name, password, role_id, active, system, project_id)
+    (id, name, password, role_id, active, system, project_id)
 VALUES
-    ('admin', '$2y$10$FZSRqIGNKq64P3Rz27jlzuKuSZ9Rik9qHnqk5zH2Z7d67.erqaNhy', CURRVAL('role_id_seq'), TRUE, TRUE, CURRVAL('project_id_seq'));
+    (1, 'admin', '$2y$10$FZSRqIGNKq64P3Rz27jlzuKuSZ9Rik9qHnqk5zH2Z7d67.erqaNhy', 1, TRUE, TRUE, 1);
 
 -- ---------------------------------------------------------------------------------------------------------------------
 
