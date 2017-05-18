d33559e664ed5314a11e09c8e6775498e2a8f8f4
Ayhan Akilli <info@ayhan.biz>
2017-05-18 12:50:50 +0200

    Formatting

--- src/account.php
+++ src/account.php
@@ -116,9 +116,7 @@ function allowed(string $key): bool
         return false;
     }
 
-    return !empty($data[$key]['call']) && call($data[$key]['call'])
-        || account_admin()
-        || in_array($key, account('privilege') ?? []);
+    return !empty($data[$key]['call']) && call($data[$key]['call']) || account_admin() || in_array($key, account('privilege') ?? []);
 }
 
 /**
