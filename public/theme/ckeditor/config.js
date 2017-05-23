52eff3955ee4dc56ed8d901c3bfd63a3649f5f96
Ayhan Akilli <info@ayhan.biz>
2017-05-23 23:19:27 +0200

    Remove unneeded RTE buttons

--- public/theme/ckeditor/config.js
+++ public/theme/ckeditor/config.js
@@ -14,7 +14,7 @@ CKEDITOR.editorConfig = function (config) {
         {name: 'others'},
         {name: 'about'}
     ];
-    config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Anchor,Strike,Subscript,Superscript';
+    config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Anchor,Strike,Subscript,Superscript,Indent,Outdent';
     config.removeDialogTabs = 'link:advanced;link:target';
     config.height = '30rem';
     config.disableNativeSpellChecker = true;
