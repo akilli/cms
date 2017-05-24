86a8b66c2a3038d3527bc139f0af57e5e366725a
Ayhan Akilli <info@ayhan.biz>
2017-05-24 09:25:35 +0200

    Revert: Remove unneeded RTE styles

---
+++ public/theme/ckeditor/styles.js
@@ -0,0 +1,17 @@
+CKEDITOR.stylesSet.add('default', [
+    {name: 'Special Container', element: 'div', attributes: {'class': 'special'}},
+    {name: 'Marker', element: 'span', attributes: {'class': 'marker'}},
+    {name: 'Big', element: 'big'},
+    {name: 'Small', element: 'small'},
+    {name: 'Typewriter', element: 'tt'},
+    {name: 'Computer Code', element: 'code'},
+    {name: 'Keyboard Phrase', element: 'kbd'},
+    {name: 'Sample Text', element: 'samp'},
+    {name: 'Variable', element: 'var'},
+    {name: 'Deleted Text', element: 'del'},
+    {name: 'Inserted Text', element: 'ins'},
+    {name: 'Cited Work', element: 'cite'},
+    {name: 'Inline Quotation', element: 'q'},
+    {name: 'Styled Image (left)', element: 'img', attributes: {'class': 'left'}},
+    {name: 'Styled Image (right)', element: 'img', attributes: {'class': 'right'}}
+]);
