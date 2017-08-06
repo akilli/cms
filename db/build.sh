8e29db66ea3cf6dd8a62047f3fd53b5f19fbbf2a
Ayhan Akilli <info@ayhan.biz>
2017-08-06 14:37:56 +0200

    Refactor Db jenkinsfile + scripts

--- db/build.sh
+++ db/build.sh
@@ -2,4 +2,17 @@
 
 set -e
 
-docker run --rm -v $(pwd):/backup -v cms_data:/app -v cms_db:/data akilli/base tar cvf /backup/db.tar /app /data
+#
+# Backup & Build
+#
+#DB=$(realpath "$(dirname "$0")")
+#docker run --rm -v $DB:/backup -v cms_data:/app -v cms_db:/data akilli/base tar cvf /backup/db.tar /app /data
+#docker build -t registry.test.eqmh.de/cms-data $DB
+
+#
+# Commit
+#
+CONT=$(docker run -d -it -v cms_data:/cms-data -v cms_db:/cms-db --entrypoint ash akilli/postgres)
+docker exec $CONT ash -c 'rm -rf /app /data && cp -R /cms-data /app && cp -R /cms-db /data && chown -R app:app /app /data'
+docker commit $CONT registry.test.eqmh.de/cms-data
+docker rm -f $CONT
--- db/setup.sh
+++ db/setup.sh
@@ -6,19 +6,13 @@ DB=$(realpath "$(dirname "$0")")
 CMS=$(realpath "$(dirname "$0")/..")
 
 #
-# Directories
-#
-rm -rf $DB/app $DB/data || true
-mkdir $DB/app $DB/data
-
-#
 # Dump
 #
-CONT=$(docker run -d -v $CMS/data/pg.sql:/pg.sql -v $DB/data:/data akilli/postgres)
+CONT=$(docker run -d -v $DB:/backup -v $CMS/data/pg.sql:/pg.sql akilli/postgres)
 sleep 10
 docker exec -u app $CONT psql -f /pg.sql app
-docker stop $CONT
-docker rm $CONT
+docker exec $CONT tar cvf /backup/db.tar /app /data
+docker rm -f $CONT
 
 #
 # Build
@@ -28,4 +22,4 @@ docker build -t registry.test.eqmh.de/cms-data $DB
 #
 # Clean
 #
-rm -rf $DB/app $DB/data
+rm $DB/db.tar
