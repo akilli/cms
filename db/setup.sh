1b08107b9b151a0fde67916a08e44d53799fd6d8
Ayhan Akilli <info@ayhan.biz>
2017-08-20 14:46:02 +0200

    Move pg.sql to db directory

--- db/setup.sh
+++ db/setup.sh
@@ -3,14 +3,13 @@
 set -e
 
 DB=$(realpath "$(dirname "$0")")
-CMS=$(realpath "$(dirname "$0")/..")
 
 #
 # Dump
 #
-CONT=$(docker run -d -v $DB:/backup -v $CMS/data/pg.sql:/pg.sql akilli/postgres)
+CONT=$(docker run -d -v $DB:/backup akilli/postgres)
 sleep 10
-docker exec -u app $CONT psql -f /pg.sql app
+docker exec -u app $CONT psql -f /backup/pg.sql app
 docker exec $CONT tar cvf /backup/db.tar /app /data
 docker rm -f $CONT
 
