df9c80f538b0f6602915407b2f430d7f87ee2082
Ayhan Akilli <info@ayhan.biz>
2017-10-10 15:43:30 +0200

    Refactor resolve()

--- src/app.php
+++ src/app.php
@@ -124,21 +124,5 @@ function logger(string $msg): void
  */
 function resolve(string $path): string
 {
-    if (strpos($path, '*') === false) {
-        return $path;
-    }
-
-    $parts = explode('/', $path);
-
-    // Wildcard for Entity Part
-    if ($parts[0] === '*') {
-        $parts[0] = request('entity');
-    }
-
-    // Wildcard for Action Part
-    if (!empty($parts[1]) && $parts[1] === '*') {
-        $parts[1] = request('action');
-    }
-
-    return implode('/', $parts);
+    return preg_replace(['#^\*/#', '#^([^/]+)/\*($|/)#'], [request('entity') . '/', '$1/' . request('action') . '$2'], $path);
 }
