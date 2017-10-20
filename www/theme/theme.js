270b884179922efe82de18b71cfa02cdf6c8273f
Ayhan Akilli <info@ayhan.biz>
2017-10-20 13:08:28 +0200

    Clean up

--- www/theme/theme.js
+++ www/theme/theme.js
@@ -24,14 +24,10 @@
         }
 
         // Rich Text Editor
-        const rte = document.querySelectorAll('[data-type=rte]');
+        const rte = document.querySelectorAll('textarea[data-type=rte]');
 
         for (let i = 0; i < rte.length; i++) {
-            ClassicEditor
-                .create(rte[i])
-                .catch(error => {
-                    console.error(error);
-                });
+            ClassicEditor.create(rte[i]);
         }
     });
 })(document, app, ClassicEditor);
