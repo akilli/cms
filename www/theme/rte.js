538877d61a8dafb59a3b5742efa2c396beec38f4
Ayhan Akilli <info@ayhan.biz>
2017-11-09 22:54:47 +0100

    customConfig: ''

--- www/theme/rte.js
+++ www/theme/rte.js
@@ -2,14 +2,39 @@
 
 (function (document, CKEDITOR) {
     document.addEventListener('DOMContentLoaded', () => {
+        const cfg = {
+            customConfig: '',
+            disableNativeSpellChecker: true,
+            extraPlugins: 'media',
+            height: '30rem',
+            mediaURL: location.protocol + '//' + location.hostname + '/media/browser/image',
+            removeButtons: 'Cut,Copy,Paste,Undo,Redo,Anchor,Strike,Subscript,Superscript,Indent,Outdent',
+            removeDialogTabs: 'link:advanced;link:target',
+            stylesSet: false,
+            toolbarGroups: [
+                {name: 'document', groups: ['mode', 'document', 'doctools']},
+                {name: 'clipboard', groups: ['clipboard', 'undo']},
+                {name: 'editing', groups: ['find', 'selection', 'spellchecker']},
+                {name: 'forms'},
+                {name: 'basicstyles', groups: ['basicstyles', 'cleanup']},
+                {name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi']},
+                {name: 'links'},
+                {name: 'insert'},
+                {name: 'styles'},
+                {name: 'colors'},
+                {name: 'tools'},
+                {name: 'others'},
+                {name: 'about'}
+            ]
+        };
         const rte = document.querySelectorAll('[data-type=rte]');
 
         for (let i = 0; i < rte.length; i++) {
             if (rte[i].tagName.toLowerCase() === 'textarea') {
-                CKEDITOR.replace(rte[i]);
+                CKEDITOR.replace(rte[i], cfg);
             } else {
                 rte[i].setAttribute('contenteditable', true);
-                CKEDITOR.inline(rte[i]);
+                CKEDITOR.inline(rte[i], cfg);
             }
         }
     });
