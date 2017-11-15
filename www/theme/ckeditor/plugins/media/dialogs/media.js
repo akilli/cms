df5a5b8d6642f3dd4fc5179b397ce5a6ea6a7c60
Ayhan Akilli <info@ayhan.biz>
2017-11-15 14:41:26 +0100

    Minimal impl of search filter

--- www/theme/ckeditor/plugins/media/dialogs/media.js
+++ www/theme/ckeditor/plugins/media/dialogs/media.js
@@ -1,6 +1,30 @@
 'use strict';
 
 (function (document, CKEDITOR) {
+    function list(q, editor, dialog) {
+        get(editor.config.mediaURL + (q ? '?q=' + q : ''))
+            .then(data => {
+                const media = JSON.parse(data);
+                const browser = document.querySelector('#ck-media');
+                browser.innerHTML = '';
+
+                for (let i = 0; i < media.length; i++) {
+                    let fig = document.createElement('figure');
+                    fig.setAttribute('class', 'media');
+                    fig.appendChild(element(media[i]));
+                    fig.appendChild(document.createElement('figcaption'));
+                    fig.addEventListener('click', () => {
+                        editor.insertHtml(fig.outerHTML);
+                        dialog.hide();
+                    });
+                    browser.appendChild(fig);
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
     function get(url) {
         return new Promise((resolve, reject) => {
             const xhr = new XMLHttpRequest();
@@ -52,27 +76,10 @@
 
             onShow: function () {
                 const dialog = this;
-                get(editor.config.mediaURL)
-                    .then(data => {
-                        const media = JSON.parse(data);
-                        const browser = document.querySelector('#ck-media');
-                        browser.innerHTML = '';
-
-                        for (let i = 0; i < media.length; i++) {
-                            let fig = document.createElement('figure');
-                            fig.setAttribute('class', 'media');
-                            fig.appendChild(element(media[i]));
-                            fig.appendChild(document.createElement('figcaption'));
-                            fig.addEventListener('click', () => {
-                                editor.insertHtml(fig.outerHTML);
-                                dialog.hide();
-                            });
-                            browser.appendChild(fig);
-                        }
-                    })
-                    .catch(error => {
-                        console.log(error);
-                    });
+                list('', editor, dialog);
+                document.querySelector('#ck-media-top input[type=search]').addEventListener('keyup', function () {
+                    list(this.value, editor, dialog);
+                });
             }
         };
     });
