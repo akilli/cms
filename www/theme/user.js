e3bfb74b5740f2afdef92c23b82a8f363b6acb0c
Ayhan Akilli <info@ayhan.biz>
2017-12-06 17:50:05 +0100

    Clean up

--- www/theme/user.js
+++ www/theme/user.js
@@ -2,37 +2,29 @@
 
 (function (window, document, app) {
     document.addEventListener('DOMContentLoaded', function () {
-        let item;
-
         // Input password autocomplete fix
-        item = document.querySelectorAll('input[type=password]');
-
-        for (let i = 0; i < item.length; i++) {
-            item[i].setAttribute('readonly', true);
-            item[i].addEventListener('focus', function () {
+        document.querySelectorAll('input[type=password]').forEach(function (item) {
+            item.setAttribute('readonly', true);
+            item.addEventListener('focus', function () {
                 this.removeAttribute('readonly');
-            })
-        }
+            });
+        });
 
         // Delete buttons and links
-        item = document.querySelectorAll('.delete');
-
-        for (let i = 0; i < del.length; i++) {
-            item[i].addEventListener('click', function (event) {
+        document.querySelectorAll('.delete').forEach(function (item) {
+            item.addEventListener('click', function (event) {
                 if (!confirm(app.i18n('Please confirm delete operation'))) {
                     event.preventDefault();
                 }
-            })
-        }
+            });
+        });
 
         // RTE browser
-        item = document.querySelectorAll('span.rte');
-
-        for (let i = 0; i < item.length; i++) {
-            item[i].addEventListener('click', function () {
+        document.querySelectorAll('span.rte').forEach(function (item) {
+            item.addEventListener('click', function () {
                 window.opener.CKEDITOR.tools.callFunction(this.getAttribute('data-rte'), this.getAttribute('data-url'));
                 window.close();
-            })
-        }
+            });
+        });
     });
 })(window, document, app);
