f29613113c68f9b5d508a7958101d9cd81496081
Ayhan Akilli <info@ayhan.biz>
2017-12-06 09:35:57 +0100

    Re-use var

--- www/theme/user.js
+++ www/theme/user.js
@@ -2,21 +2,23 @@
 
 (function (window, document, app) {
     document.addEventListener('DOMContentLoaded', function () {
-        // Input password autocomplete fix
-        const pwd = document.querySelectorAll('input[type=password]');
+        let item;
 
-        for (let i = 0; i < pwd.length; i++) {
-            pwd[i].setAttribute('readonly', true);
-            pwd[i].addEventListener('focus', function () {
+        // Input password autocomplete fix
+        item = document.querySelectorAll('input[type=password]');
+
+        for (let i = 0; i < item.length; i++) {
+            item[i].setAttribute('readonly', true);
+            item[i].addEventListener('focus', function () {
                 this.removeAttribute('readonly');
             })
         }
 
         // Delete buttons and links
-        const del = document.querySelectorAll('.delete');
+        item = document.querySelectorAll('.delete');
 
         for (let i = 0; i < del.length; i++) {
-            del[i].addEventListener('click', function (event) {
+            item[i].addEventListener('click', function (event) {
                 if (!confirm(app.i18n('Please confirm delete operation'))) {
                     event.preventDefault();
                 }
@@ -24,10 +26,10 @@
         }
 
         // RTE browser
-        const rte = document.querySelectorAll('span.rte');
+        item = document.querySelectorAll('span.rte');
 
-        for (let i = 0; i < rte.length; i++) {
-            rte[i].addEventListener('click', function () {
+        for (let i = 0; i < item.length; i++) {
+            item[i].addEventListener('click', function () {
                 window.opener.CKEDITOR.tools.callFunction(this.getAttribute('data-rte'), this.getAttribute('data-url'));
                 window.close();
             })
