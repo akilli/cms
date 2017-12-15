423abf546deb92b7e3ebf9365e6dfb47169fa682
Ayhan Akilli <info@ayhan.biz>
2017-12-15 19:42:24 +0100

    Prepare section cfg

--- cfg/section.php
+++ cfg/section.php
@@ -1,25 +1,28 @@
 <?php
 return [
     'container' => [
-        'vars' => ['tag' => null],
+        'opt' => ['tag' => null],
     ],
     'ent' => [
-        'vars' => ['ent' => null, 'crit' => [], 'opt' => [], 'act' => null],
+        'opt' => ['crit' => [], 'ent' => null, 'opt' => []],
+        'vars' => ['act' => null],
     ],
     'index' => [
-        'vars' => ['ent' => null, 'crit' => [], 'opt' => [], 'act' => null, 'params' => []],
+        'opt' => ['crit' => [], 'ent' => null, 'opt' => []],
+        'vars' => ['act' => null, 'params' => []],
     ],
     'msg' => [
         'tpl' => 'layout/msg.phtml',
-        'vars' => [],
+        'vars' => ['data' => []],
     ],
     'nav' => [
-        'vars' => ['mode' => null],
+        'opt' => ['mode' => null],
     ],
     'pager' => [
-        'vars' => ['size' => 0, 'limit' => 0, 'links' => [], 'params' => []],
+        'opt' => ['params' => []],
+        'vars' => ['limit' => 0, 'links' => [], 'size' => 0],
     ],
     'tpl' => [
-        'vars' => [],
+        'vars' => ['id' => null, 'tpl' => null],
     ],
 ];
