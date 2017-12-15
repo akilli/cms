b2caa3bc56484136945df87b40184635a676b0ae
Ayhan Akilli <info@ayhan.biz>
2017-12-15 12:14:25 +0100

    time format

--- cfg/app.php
+++ cfg/app.php
@@ -1,9 +1,9 @@
 <?php
 return [
     'date' => 'd.m.Y',
-    'datetime' => 'd.m.Y H:i',
+    'datetime' => 'd.m.Y (H:i \U\h\r)',
     'limit' => 20,
     'name' => 'akıllı CMS',
     'pager' => 5,
-    'time' => 'H:i',
+    'time' => 'H:i \U\h\r',
 ];
