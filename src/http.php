51bc3211d90c14a363698b89cf0ddc0d538f7a28
Ayhan Akilli <info@ayhan.biz>
2017-12-22 09:15:12 +0100

    Revert: Remove support for non integer IDs, as not needed... yet vol. 3 => (int) req.id

--- src/http.php
+++ src/http.php
@@ -44,7 +44,7 @@ function req(string $key)
         $parts = explode('/', trim(app\rewrite($req['url']), '/'));
         $req['ent'] = array_shift($parts);
         $req['act'] = array_shift($parts);
-        $req['id'] = $parts ? (int) array_shift($parts) : null;
+        $req['id'] = array_shift($parts);
         $req['path'] = $req['ent'] . '/' . $req['act'];
     }
 
