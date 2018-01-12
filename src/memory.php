cc154f201c381ed26910125ecab90594a6dd152f
Ayhan Akilli <info@ayhan.biz>
2018-01-12 15:46:20 +0100

    Replace mail entity type with memory

---
+++ src/memory.php
@@ -0,0 +1,70 @@
+<?php
+declare(strict_types = 1);
+
+namespace memory;
+
+use app;
+use arr;
+
+/**
+ * Load entity
+ */
+function load(array $ent, array $crit = [], array $opt = []): array
+{
+    $db = app\data('memory.' . $ent['id']);
+
+    if (!$db || $crit && !($db = arr\crit($db, $crit))) {
+        return $opt['mode'] === 'size' ? [0] : [];
+    }
+
+    if ($opt['mode'] === 'size') {
+        return [count($db)];
+    }
+
+    if ($opt['order']) {
+        $db = arr\order($db, $opt['order']);
+    }
+
+    if ($opt['limit'] > 0) {
+        $db = array_slice($db, $opt['offset'], $opt['limit'], true);
+    }
+
+    if ($opt['select']) {
+        $base = array_fill_keys($opt['select'], null);
+
+        foreach ($db as $key => $item) {
+            $db[$key] = array_intersect_key($item, $base);
+        }
+    }
+
+    if ($opt['mode'] === 'one') {
+        return current($db) ?: [];
+    }
+
+    return $db;
+}
+
+/**
+ * Save entity
+ */
+function save(array $data): array
+{
+    $db = & app\data('memory.' . $data['_ent']['id']);
+
+    if (!$data['_old'] && !isset($data['id'])) {
+        $data['id'] = app\hash();
+    }
+
+    $db[$data['id']] = $data;
+
+    return $data;
+}
+
+/**
+ * Delete entity
+ */
+function delete(array $data): void
+{
+    $db = & app\data('memory.' . $data['_ent']['id']);
+    unset($db[$data['id']]);
+}
