ce27744984a7b0df005471cacbe0c63abf0af141
Ayhan Akilli <info@ayhan.biz>
2018-01-18 13:29:26 +0100

    Re-add url entity part 2

--- cfg/toolbar.php
+++ cfg/toolbar.php
@@ -2,8 +2,9 @@
 return [
     'page/admin' => ['name' => 'Pages', 'sort' => 100],
     'file/admin' => ['name' => 'Files', 'sort' => 200],
-    'account/admin' => ['name' => 'Accounts', 'sort' => 300],
-    'role/admin' => ['name' => 'Roles', 'sort' => 400],
-    'account/password' => ['name' => 'Password', 'sort' => 500],
-    'account/logout' => ['name' => 'Logout', 'sort' => 600],
+    'url/admin' => ['name' => 'URL', 'sort' => 300],
+    'account/admin' => ['name' => 'Accounts', 'sort' => 400],
+    'role/admin' => ['name' => 'Roles', 'sort' => 500],
+    'account/password' => ['name' => 'Password', 'sort' => 600],
+    'account/logout' => ['name' => 'Logout', 'sort' => 700],
 ];
