07bacbe59dac4856160090542a3885ca0391d087
Ayhan Akilli <info@ayhan.biz>
2018-01-19 11:26:51 +0100

    Add article entity

--- cfg/priv.php
+++ cfg/priv.php
@@ -27,6 +27,12 @@ return [
     'app/js' => [
         'priv' => 'account-user',
     ],
+    'article/index' => [
+        'active' => false,
+    ],
+    'article/view' => [
+        'active' => false,
+    ],
     'file/asset' => [
         'active' => false,
     ],
