6e56a66788a0e4be2312291e812cda8a08786083
Ayhan Akilli <info@ayhan.biz>
2018-01-26 10:56:06 +0100

    Forward role/account admin/edit/delete privileges to _all_

--- cfg/priv.php
+++ cfg/priv.php
@@ -9,6 +9,15 @@ return [
     'account-user' => [
         'assignable' => false,
     ],
+    'account/admin' => [
+        'priv' => '_all_',
+    ],
+    'account/delete' => [
+        'priv' => '_all_',
+    ],
+    'account/edit' => [
+        'priv' => '_all_',
+    ],
     'account/login' => [
         'priv' => 'account-guest',
     ],
@@ -41,5 +50,14 @@ return [
     ],
     'page/index' => [
         'active' => false,
+    ],
+    'role/admin' => [
+        'priv' => '_all_',
+    ],
+    'role/delete' => [
+        'priv' => '_all_',
+    ],
+    'role/edit' => [
+        'priv' => '_all_',
     ],
 ];
