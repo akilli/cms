1ba0390a11028d5287209b3138daa3086ec352c1
Ayhan Akilli <info@ayhan.biz>
2018-02-21 22:13:42 +0100

    Minimal style adjustements

--- cfg/app.php
+++ cfg/app.php
@@ -2,7 +2,7 @@
 return [
     'date' => 'd.m.Y',
     'datetime' => 'd.m.Y (H:i \U\h\r)',
-    'limit' => 20,
+    'limit' => 10,
     'name' => 'akıllı CMS',
     'pager' => 5,
     'time' => 'H:i \U\h\r',
