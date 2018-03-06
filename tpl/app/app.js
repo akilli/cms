190b389e5b4e4bc363e7b564e5c1c3bde613c29b
Ayhan Akilli <info@ayhan.biz>
2018-03-06 23:56:42 +0100

    Fix regression

--- tpl/app/app.js
+++ tpl/app/app.js
@@ -1,15 +1,16 @@
 <?php
 namespace app;
 /** @var callable $§ */
-$cfg = ['i18' => cfg('i18n')];
 $opt = cfg('opt');
-$cfg['file'] = array_fill_keys($opt['audio'], 'audio') + array_fill_keys($opt['image'], 'img') + array_fill_keys($opt['video'], 'video');
-ksort($cfg['file']);
+$file = array_fill_keys($opt['audio'], 'audio') + array_fill_keys($opt['image'], 'img') + array_fill_keys($opt['video'], 'video');
 ?>
 'use strict';
 
 const app = {
-    cfg: <?=json_encode($cfg);?>,
+    cfg: {
+        file: <?=json_encode($file);?>,
+        i18n: <?=json_encode(cfg('i18n'));?>
+    },
     i18n: function (key, ...args) {
         key = this.cfg.i18n[key] ? this.cfg.i18n[key] : key;
         args.map(function (i) {
