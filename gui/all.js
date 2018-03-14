bd65df59cf5347fee025b5bd59aa1faa0913ee05
Ayhan Akilli <info@ayhan.biz>
2018-03-14 17:25:59 +0100

    Add IE11 support _public_

--- gui/all.js
+++ gui/all.js
@@ -3,25 +3,26 @@
 (function (document) {
     document.addEventListener('DOMContentLoaded', function () {
         // Multi-checkbox required fix
-        document.querySelectorAll('form').forEach(function (form) {
-            form.querySelectorAll('input[type=checkbox][required][multiple]').forEach(function (item) {
-                item.addEventListener('change', function () {
-                    let sel = 'input[type=checkbox][multiple][name="' + this.name + '"]';
-                    let call;
+        const forms = document.querySelectorAll('form');
 
-                    if (form.querySelectorAll(sel + ':checked').length > 0) {
-                        call = function (i) {
-                            i.removeAttribute('required');
-                        };
-                    } else {
-                        call = function (i) {
-                            i.setAttribute('required', 'required');
-                        };
+        for (let a = 0; a < forms.length; a++) {
+            let sel = 'input[type=checkbox][multiple]';
+            let multi = forms[a].querySelectorAll(sel + '[required]');
+
+            for (let b = 0; b < multi.length; b++) {
+                multi[b].addEventListener('change', function (ev) {
+                    let req = !!this.form.querySelector(sel + '[name="' + this.name + '"]:checked');
+                    let sib = this.form.querySelectorAll(sel + '[name="' + this.name + '"]');
+
+                    for (let c = 0; c < sib.length; c++) {
+                        if (req) {
+                            sib[c].removeAttribute('required');
+                        } else {
+                            sib[c].setAttribute('required', 'required');
+                        }
                     }
-
-                    form.querySelectorAll(sel).forEach(call);
                 });
-            });
-        });
+            }
+        }
     });
 })(document);
