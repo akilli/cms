288fb8549998fc3dd2ed54b4d682982e455c35a4
Ayhan Akilli <info@ayhan.biz>
2018-03-28 10:35:29 +0200

    Revert: Ignore invalid blocks when value is only read

--- src/app.php
+++ src/app.php
@@ -228,10 +228,6 @@ function layout(string $id = null, array $§ = null): ?array
         return $data;
     }
 
-    if (empty($data[$id]) && $§ === null) {
-        return null;
-    }
-
     if (empty($data[$id])) {
         if (empty($§['type']) || !($type = cfg('block', $§['type']))) {
             throw new DomainException(i18n('Invalid block %s', $id));
