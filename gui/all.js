227b99e3c5d41024bff1c73d60a2fbd69c6c083e
Ayhan Akilli <info@ayhan.biz>
2018-04-22 09:39:49 +0200

    Use setTimeout() for flickity

--- gui/all.js
+++ gui/all.js
@@ -74,40 +74,42 @@
                 return;
             }
 
-            const sliderNav = item.querySelector('.slider-nav');
-            const flickity = new Flickity(sliderItems, {
-                autoPlay: true,
-                imagesLoaded: true,
-                pageDots: item.classList.contains('slider-dots'),
-                prevNextButtons: item.classList.contains('slider-prevnext'),
-                wrapAround: true
-            });
-            item.addEventListener('mouseenter', function () {
-                flickity.stopPlayer();
-            });
-            item.addEventListener('mouseleave', function () {
-                flickity.playPlayer();
-            });
-
-            if (!!sliderNav) {
-                const sliderButton = sliderNav.getElementsByTagName('button');
-
-                Array.prototype.forEach.call(sliderButton, function (button) {
-                    button.addEventListener('click', function (ev) {
-                        flickity.select(Array.prototype.indexOf.call(sliderButton, ev.target));
-                    });
+            setTimeout(function() {
+                const sliderNav = item.querySelector('.slider-nav');
+                const flickity = new Flickity(sliderItems, {
+                    autoPlay: true,
+                    imagesLoaded: true,
+                    pageDots: item.classList.contains('slider-dots'),
+                    prevNextButtons: item.classList.contains('slider-prevnext'),
+                    wrapAround: true
+                });
+                item.addEventListener('mouseenter', function () {
+                    flickity.stopPlayer();
+                });
+                item.addEventListener('mouseleave', function () {
+                    flickity.playPlayer();
                 });
 
-                flickity.on('select', function (current) {
-                    Array.prototype.forEach.call(sliderButton, function (button, index) {
-                        if (index === current) {
-                            button.classList.add('is-selected');
-                        } else {
-                            button.classList.remove('is-selected');
-                        }
+                if (!!sliderNav) {
+                    const sliderButton = sliderNav.getElementsByTagName('button');
+
+                    Array.prototype.forEach.call(sliderButton, function (button) {
+                        button.addEventListener('click', function (ev) {
+                            flickity.select(Array.prototype.indexOf.call(sliderButton, ev.target));
+                        });
                     });
-                });
-            }
+
+                    flickity.on('select', function (current) {
+                        Array.prototype.forEach.call(sliderButton, function (button, index) {
+                            if (index === current) {
+                                button.classList.add('is-selected');
+                            } else {
+                                button.classList.remove('is-selected');
+                            }
+                        });
+                    });
+                }
+            });
         });
 
         // Gallery dialog
