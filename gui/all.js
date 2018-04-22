1e91ff6df85092192d11608719c304d32687365c
Ayhan Akilli <info@ayhan.biz>
2018-04-22 15:47:47 +0200

    Revert: Use setTimeout() for flickity

--- gui/all.js
+++ gui/all.js
@@ -74,42 +74,40 @@
                 return;
             }
 
-            setTimeout(function() {
-                const sliderNav = item.querySelector('.slider-nav');
-                const flickity = new Flickity(sliderItems, {
-                    autoPlay: true,
-                    imagesLoaded: true,
-                    pageDots: item.classList.contains('slider-dots'),
-                    prevNextButtons: item.classList.contains('slider-prevnext'),
-                    wrapAround: true
-                });
-                item.addEventListener('mouseenter', function () {
-                    flickity.stopPlayer();
-                });
-                item.addEventListener('mouseleave', function () {
-                    flickity.playPlayer();
-                });
-
-                if (!!sliderNav) {
-                    const sliderButton = sliderNav.getElementsByTagName('button');
-
-                    Array.prototype.forEach.call(sliderButton, function (button) {
-                        button.addEventListener('click', function (ev) {
-                            flickity.select(Array.prototype.indexOf.call(sliderButton, ev.target));
-                        });
-                    });
-
-                    flickity.on('select', function (current) {
-                        Array.prototype.forEach.call(sliderButton, function (button, index) {
-                            if (index === current) {
-                                button.classList.add('is-selected');
-                            } else {
-                                button.classList.remove('is-selected');
-                            }
-                        });
-                    });
-                }
+            const sliderNav = item.querySelector('.slider-nav');
+            const flickity = new Flickity(sliderItems, {
+                autoPlay: true,
+                imagesLoaded: true,
+                pageDots: item.classList.contains('slider-dots'),
+                prevNextButtons: item.classList.contains('slider-prevnext'),
+                wrapAround: true
             });
+            item.addEventListener('mouseenter', function () {
+                flickity.stopPlayer();
+            });
+            item.addEventListener('mouseleave', function () {
+                flickity.playPlayer();
+            });
+
+            if (!!sliderNav) {
+                const sliderButton = sliderNav.getElementsByTagName('button');
+
+                Array.prototype.forEach.call(sliderButton, function (button) {
+                    button.addEventListener('click', function (ev) {
+                        flickity.select(Array.prototype.indexOf.call(sliderButton, ev.target));
+                    });
+                });
+
+                flickity.on('select', function (current) {
+                    Array.prototype.forEach.call(sliderButton, function (button, index) {
+                        if (index === current) {
+                            button.classList.add('is-selected');
+                        } else {
+                            button.classList.remove('is-selected');
+                        }
+                    });
+                });
+            }
         });
 
         // Gallery dialog
