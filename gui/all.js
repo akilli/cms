d8ba2129f5f6a7688f72025e1fdea867017a1aea
Ayhan Akilli <info@ayhan.biz>
2018-04-30 16:47:52 +0200

    Flickity config

--- gui/all.js
+++ gui/all.js
@@ -66,50 +66,6 @@
             });
         });
 
-        // Slider
-        Array.prototype.forEach.call(document.getElementsByClassName('slider'), function (item) {
-            const sliderItems = item.querySelector('.slider-items');
-
-            if (!sliderItems) {
-                return;
-            }
-
-            const sliderNav = item.querySelector('.slider-nav');
-            const flickity = new Flickity(sliderItems, {
-                autoPlay: true,
-                imagesLoaded: true,
-                pageDots: item.classList.contains('slider-dots'),
-                prevNextButtons: item.classList.contains('slider-prevnext'),
-                wrapAround: true
-            });
-            item.addEventListener('mouseenter', function () {
-                flickity.stopPlayer();
-            });
-            item.addEventListener('mouseleave', function () {
-                flickity.playPlayer();
-            });
-
-            if (!!sliderNav) {
-                const sliderButton = sliderNav.getElementsByTagName('button');
-
-                Array.prototype.forEach.call(sliderButton, function (button) {
-                    button.addEventListener('click', function (ev) {
-                        flickity.select(Array.prototype.indexOf.call(sliderButton, ev.target));
-                    });
-                });
-
-                flickity.on('select', function (current) {
-                    Array.prototype.forEach.call(sliderButton, function (button, index) {
-                        if (index === current) {
-                            button.classList.add('is-selected');
-                        } else {
-                            button.classList.remove('is-selected');
-                        }
-                    });
-                });
-            }
-        });
-
         // Gallery dialog
         Array.prototype.forEach.call(document.querySelectorAll('.gallery .items > *'), function (item) {
             item.addEventListener('click', function (e) {
@@ -169,6 +125,50 @@
     });
 
     window.addEventListener('load', function () {
+        // Slider
+        Array.prototype.forEach.call(document.getElementsByClassName('slider'), function (item) {
+            const sliderItems = item.querySelector('.slider-items');
+
+            if (!sliderItems) {
+                return;
+            }
+
+            const sliderNav = item.querySelector('.slider-nav');
+            const flickity = new Flickity(sliderItems, {
+                autoPlay: true,
+                imagesLoaded: true,
+                pageDots: item.classList.contains('slider-dots'),
+                prevNextButtons: item.classList.contains('slider-prevnext'),
+                wrapAround: true
+            });
+            item.addEventListener('mouseenter', function () {
+                flickity.stopPlayer();
+            });
+            item.addEventListener('mouseleave', function () {
+                flickity.playPlayer();
+            });
+
+            if (!!sliderNav) {
+                const sliderButton = sliderNav.getElementsByTagName('button');
+
+                Array.prototype.forEach.call(sliderButton, function (button) {
+                    button.addEventListener('click', function (ev) {
+                        flickity.select(Array.prototype.indexOf.call(sliderButton, ev.target));
+                    });
+                });
+
+                flickity.on('select', function (current) {
+                    Array.prototype.forEach.call(sliderButton, function (button, index) {
+                        if (index === current) {
+                            button.classList.add('is-selected');
+                        } else {
+                            button.classList.remove('is-selected');
+                        }
+                    });
+                });
+            }
+        });
+
         // Sticky navigation polyfill
         const nav = document.querySelector('#menu.sticky');
 
