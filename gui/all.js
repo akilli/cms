571ab4f3064b288ea09d963fc8cf8090864e4e76
Ayhan Akilli <info@ayhan.biz>
2018-05-02 15:37:48 +0200

    Extract slider.js from all.js

--- gui/all.js
+++ gui/all.js
@@ -1,6 +1,6 @@
 'use strict';
 
-(function (document, window) {
+(function (document) {
     document.addEventListener('DOMContentLoaded', function () {
         // Multi-checkbox required fix
         const sel = 'input[type=checkbox][multiple]';
@@ -19,50 +19,4 @@
             });
         });
     });
-
-    window.addEventListener('load', function () {
-        // Slider
-        Array.prototype.forEach.call(document.getElementsByClassName('slider'), function (item) {
-            const sliderItems = item.querySelector('.slider-items');
-
-            if (!sliderItems) {
-                return;
-            }
-
-            const sliderNav = item.querySelector('.slider-nav');
-            const flickity = new Flickity(sliderItems, {
-                autoPlay: true,
-                imagesLoaded: true,
-                pageDots: item.classList.contains('slider-dots'),
-                prevNextButtons: item.classList.contains('slider-prevnext'),
-                wrapAround: true
-            });
-            item.addEventListener('mouseenter', function () {
-                flickity.stopPlayer();
-            });
-            item.addEventListener('mouseleave', function () {
-                flickity.playPlayer();
-            });
-
-            if (!!sliderNav) {
-                const sliderButton = sliderNav.getElementsByTagName('button');
-
-                Array.prototype.forEach.call(sliderButton, function (button) {
-                    button.addEventListener('click', function (ev) {
-                        flickity.select(Array.prototype.indexOf.call(sliderButton, ev.target));
-                    });
-                });
-
-                flickity.on('select', function (current) {
-                    Array.prototype.forEach.call(sliderButton, function (button, index) {
-                        if (index === current) {
-                            button.classList.add('is-selected');
-                        } else {
-                            button.classList.remove('is-selected');
-                        }
-                    });
-                });
-            }
-        });
-    });
-})(document, window);
+})(document);
