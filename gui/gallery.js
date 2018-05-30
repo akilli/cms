af38ca15f1c9bdf25a9077a5ffaba322e27833d8
Ayhan Akilli <info@ayhan.biz>
2018-05-30 11:49:09 +0200

    Simplify gallery

--- gui/gallery.js
+++ gui/gallery.js
@@ -11,6 +11,21 @@
                     document.getElementById('dialog').parentElement.removeChild(document.getElementById('dialog'));
                 }
 
+                const tag = this.tagName.toLowerCase();
+                let src;
+
+                if (tag === 'img') {
+                    src = this.getAttribute('src');
+                } else if (tag === 'a' && this.hasAttribute('href')) {
+                    src = this.getAttribute('href');
+                } else {
+                    src = !!(src = this.querySelector('img')) ? src.getAttribute('src') : null;
+                }
+
+                if (!src) {
+                    return;
+                }
+
                 let current = this;
                 const dialog = document.createElement('dialog');
                 const img = document.createElement('img');
@@ -53,7 +68,7 @@
                 });
                 dialog.appendChild(next);
                 // Image
-                img.setAttribute('src', this.getAttribute('href'));
+                img.setAttribute('src', src);
                 dialog.appendChild(img);
                 // Open dialog
                 dialog.setAttribute('open', '');
