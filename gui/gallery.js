b15253afaa154038c81cff458726fe3778952f0a
Ayhan Akilli <info@ayhan.biz>
2018-05-30 13:00:15 +0200

    Revert: Simplify gallery

--- gui/gallery.js
+++ gui/gallery.js
@@ -11,21 +11,6 @@
                     document.getElementById('dialog').parentElement.removeChild(document.getElementById('dialog'));
                 }
 
-                const tag = this.tagName.toLowerCase();
-                let src;
-
-                if (tag === 'img') {
-                    src = this.getAttribute('src');
-                } else if (tag === 'a' && this.hasAttribute('href')) {
-                    src = this.getAttribute('href');
-                } else {
-                    src = !!(src = this.querySelector('img')) ? src.getAttribute('src') : null;
-                }
-
-                if (!src) {
-                    return;
-                }
-
                 let current = this;
                 const dialog = document.createElement('dialog');
                 const img = document.createElement('img');
@@ -68,7 +53,7 @@
                 });
                 dialog.appendChild(next);
                 // Image
-                img.setAttribute('src', src);
+                img.setAttribute('src', this.getAttribute('href'));
                 dialog.appendChild(img);
                 // Open dialog
                 dialog.setAttribute('open', '');
