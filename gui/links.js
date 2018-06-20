514a0b69dcc1ab881e428bd320c25874fb65f599
Ayhan Akilli <info@ayhan.biz>
2018-06-20 12:26:16 +0200

    Add links helper function

---
+++ gui/links.js
@@ -0,0 +1,24 @@
+/**
+ * Links
+ */
+'use strict';
+
+(function (document) {
+    document.addEventListener('DOMContentLoaded', function () {
+        Array.prototype.forEach.call(document.querySelectorAll('html[data-area=_public_] main a[href]:not([role])'), function (item) {
+            const href = item.getAttribute('href');
+
+            if (!!item.querySelector('img')) {
+                item.setAttribute('data-link', 'img');
+            } else if (href.indexOf('/file/') === 0) {
+                item.setAttribute('data-link', 'file');
+            } else if (href.indexOf('/') === 0) {
+                item.setAttribute('data-link', 'intern');
+            } else if (href.indexOf('mailto:') === 0) {
+                item.setAttribute('data-link', 'email');
+            } else if (href.match(/^https?:\/\//)) {
+                item.setAttribute('data-link', 'extern');
+            }
+        });
+    });
+})(document);
