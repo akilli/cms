eb6a666f1eea92bae36162e56c49abfbfceb896a
Ayhan Akilli <info@ayhan.biz>
2018-10-10 15:59:08 +0200

    Reorder

--- db/upgrade.sql
+++ db/upgrade.sql
@@ -16,9 +16,9 @@ ALTER TABLE file RENAME COLUMN ent TO entity;
 -- Page
 -- ---------------------------------------------------------------------------------------------------------------------
 
-ALTER TABLE page RENAME COLUMN ent TO entity;
-ALTER TABLE page RENAME COLUMN meta TO meta_description;
 ALTER TABLE page ADD COLUMN meta_title varchar(80) NOT NULL DEFAULT '';
+ALTER TABLE page RENAME COLUMN meta TO meta_description;
+ALTER TABLE page RENAME COLUMN ent TO entity;
 
 -- ---------------------------------------------------------------------------------------------------------------------
 
