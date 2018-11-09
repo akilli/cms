b79c41796b6e1fe2299425c2015142c0a64bc51b
Ayhan Akilli <info@ayhan.biz>
2018-11-09 09:55:57 +0100

    Clean up

--- src/app.php
+++ src/app.php
@@ -387,7 +387,6 @@ function curl(string $url, array $param = []): ?string
     }
 
     $curl = curl_init();
-
     curl_setopt_array($curl, [CURLOPT_URL => $url] + cfg('app', 'curl'));
     $result = curl_exec($curl);
     curl_close($curl);
