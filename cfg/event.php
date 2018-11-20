87a56a1009933fa08584c42552807e3e4961fc81
Ayhan Akilli <info@ayhan.biz>
2018-11-20 09:43:13 +0100

    Adjust event listener sort order

--- cfg/event.php
+++ cfg/event.php
@@ -1,42 +1,42 @@
 <?php
 return [
     'cfg.block' => [
-        'event\cfg_block' => -1,
+        'event\cfg_block' => 10,
     ],
     'cfg.entity' => [
-        'event\cfg_entity' => -1,
+        'event\cfg_entity' => 10,
     ],
     'cfg.i18n' => [
-        'event\cfg_i18n' => -1,
+        'event\cfg_i18n' => 10,
     ],
     'cfg.opt' => [
-        'event\cfg_opt' => -1,
+        'event\cfg_opt' => 10,
     ],
     'cfg.priv' => [
-        'event\cfg_priv' => -1,
+        'event\cfg_priv' => 10,
     ],
     'cfg.toolbar' => [
-        'event\cfg_toolbar' => -1,
+        'event\cfg_toolbar' => 10,
     ],
     'layout' => [
-        'event\layout' => -1,
+        'event\layout' => 10,
     ],
     'entity.postfilter' => [
-        'event\entity_postfilter' => -1,
+        'event\entity_postfilter' => 10,
     ],
     'entity.postsave' => [
-        'event\entity_postsave' => -1,
+        'event\entity_postsave' => 10,
     ],
     'entity.prefilter.id.file' => [
-        'event\entity_prefilter_file' => -1,
+        'event\entity_prefilter_file' => 10,
     ],
     'entity.postsave.id.file' => [
-        'event\entity_postsave_file' => -1,
+        'event\entity_postsave_file' => 10,
     ],
     'entity.postdelete.id.file' => [
-        'event\entity_postdelete_file' => -1,
+        'event\entity_postdelete_file' => 10,
     ],
     'entity.postfilter.id.page' => [
-        'event\entity_postfilter_page' => -1,
+        'event\entity_postfilter_page' => 10,
     ],
 ];
