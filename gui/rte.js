9df161b0e56100b633da08a935572f17ba8695c1
Ayhan Akilli <info@ayhan.biz>
2018-11-30 13:24:48 +0100

    Move rte stuff to rte js

--- gui/rte.js
+++ gui/rte.js
@@ -4,9 +4,29 @@
 'use strict';
 
 (function (document, app, CKEDITOR) {
+    const cfg = {
+        contentsCss: '',
+        customConfig: '',
+        disableNativeSpellChecker: true,
+        extraAllowedContent: 'article section(*)',
+        fillEmptyBlocks: false,
+        format_tags: 'p;h2;h3',
+        height: '30rem',
+        language: 'de',
+        mediabrowserUrl: '/file/browser',
+        removeDialogTabs: 'link:advanced;link:target',
+        stylesSet: false,
+        toolbar: [
+            {
+                name: 'all',
+                items: ['Undo', 'Redo', 'Bold', 'Italic', 'Link', 'Unlink', 'Format', 'BulletedList', 'NumberedList', 'Blockquote', 'Media', 'Table', 'Detail']
+            }
+        ]
+    };
+
     document.addEventListener('DOMContentLoaded', function () {
         Array.prototype.forEach.call(document.querySelectorAll('textarea[data-type=rte]'), function (item) {
-            CKEDITOR.replace(item, app.rte);
+            CKEDITOR.replace(item, cfg);
         });
     });
 })(document, app, CKEDITOR);
