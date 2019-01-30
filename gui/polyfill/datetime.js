b68b068483bdbe8c97f9b514378eba6d24f7e3ac
Ayhan Akilli <info@ayhan.biz>
2019-01-30 16:28:37 +0100

    Fix datetime polyfill

--- gui/polyfill/datetime.js
+++ gui/polyfill/datetime.js
@@ -19,24 +19,30 @@
         Array.prototype.forEach.call(inputs, function (item) {
             const date = document.createElement('input');
             const time = document.createElement('input');
-            const blur = function () {
-                if (date.value && date.checkValidity() && time.value && time.checkValidity()) {
+            const change = function () {
+                if (date.value && date.checkValidity()) {
+                    if (!time.value || !time.checkValidity()) {
+                        time.value = '00:00';
+                    }
+
                     item.value = date.value + 'T' + time.value;
                 } else {
+                    date.value = '';
+                    time.value = '';
                     item.value = '';
                 }
             };
-            const regex = /^(\d\d\d\d-(?:0[1-9]|1[0-2])-(?:0[1-9]|[12][0-9]|3[01]))T((?:00|[0-9]|1[0-9]|2[0-3]):(?:[0-9]|[0-5][0-9]))$/;
+            const regex = /^(\d\d\d\d-(?:0[1-9]|1[0-2])-(?:0[1-9]|[12][0-9]|3[01]))T((?:0[0-9]|1[0-9]|2[0-3]):(?:[0-9]|[0-5][0-9]))$/;
             const val = item.value.match(regex) || ['', '', ''];
 
             date.type = 'date';
             date.required = item.required;
             date.value = val[1];
-            date.addEventListener('blur', blur);
+            date.addEventListener('change', change);
             time.type = 'time';
             time.required = item.required;
             time.value = val[2];
-            time.addEventListener('blur', blur);
+            time.addEventListener('change', change);
             item.setAttribute('hidden', '');
             item.parentElement.insertBefore(date, item);
             item.parentElement.insertBefore(time, item);
