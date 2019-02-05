f4c02fa519fde2184dbf968b4abe892247ea24e6
Ayhan Akilli <info@ayhan.biz>
2019-02-05 16:31:48 +0100

    NULL

--- db/app.sql
+++ db/app.sql
@@ -56,8 +56,14 @@ CREATE FUNCTION entity_save() RETURNS trigger AS $$
             END IF;
 
             _col := _col || quote_ident(_attr);
-            _val := _val || quote_literal(jsonb_extract_path_text(_data, _attr));
-            _set := _set || quote_ident(_attr) || ' = ' || quote_literal(jsonb_extract_path_text(_data, _attr));
+
+            IF (jsonb_extract_path_text(_data, _attr) IS NULL) THEN
+                _val := _val || 'NULL';
+                _set := _set || quote_ident(_attr) || ' = ' || 'NULL';
+            ELSE
+                _val := _val || quote_literal(jsonb_extract_path_text(_data, _attr));
+                _set := _set || quote_ident(_attr) || ' = ' || quote_literal(jsonb_extract_path_text(_data, _attr));
+            END IF;
         END LOOP;
 
         IF (TG_OP = 'UPDATE') THEN
@@ -92,8 +98,14 @@ CREATE FUNCTION entity_save() RETURNS trigger AS $$
             END IF;
 
             _col := _col || quote_ident(_attr);
-            _val := _val || quote_literal(jsonb_extract_path_text(_data, _attr));
-            _set := _set || quote_ident(_attr) || ' = ' || quote_literal(jsonb_extract_path_text(_data, _attr));
+
+            IF (jsonb_extract_path_text(_data, _attr) IS NULL) THEN
+                _val := _val || 'NULL';
+                _set := _set || quote_ident(_attr) || ' = ' || 'NULL';
+            ELSE
+                _val := _val || quote_literal(jsonb_extract_path_text(_data, _attr));
+                _set := _set || quote_ident(_attr) || ' = ' || quote_literal(jsonb_extract_path_text(_data, _attr));
+            END IF;
         END LOOP;
 
         IF (TG_OP = 'UPDATE' AND (SELECT count(*) FROM block_content_ext WHERE id = OLD.id) > 0) THEN
