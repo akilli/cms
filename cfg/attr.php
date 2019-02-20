55345716810f9e3417ad28ab6ab0d6db9bb5ea85
Ayhan Akilli <info@ayhan.biz>
2019-02-20 01:37:01 +0100

    Use date as filter for datetime

--- cfg/attr.php
+++ cfg/attr.php
@@ -24,6 +24,7 @@ return [
     'datetime' => [
         'backend' => 'datetime',
         'frontend' => 'frontend\datetime',
+        'filter' => 'frontend\date',
         'validator' => 'validator\datetime',
         'viewer' => 'viewer\datetime',
     ],
