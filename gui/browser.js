4c95504bbd9287cc29351769be314df3f6196e4a
Ayhan Akilli <info@ayhan.biz>
2019-02-23 15:35:40 +0100

    Clean up

--- gui/browser.js
+++ gui/browser.js
@@ -18,7 +18,7 @@
             return;
         }
 
-        Array.prototype.forEach.call(document.querySelectorAll('html[data-action=browser] #content .block-index > article'), function (item) {
+        Array.prototype.forEach.call(document.querySelectorAll('html[data-action=browser] #content .block-index article'), function (item) {
             item.addEventListener('click', function () {
                 window.opener.postMessage({
                     id: item.getAttribute('data-id'),
