cff7487d6d490f2a4c34aee463848c1d9a2291bf
Ayhan Akilli <info@ayhan.biz>
2019-03-01 14:42:14 +0100

    Minimal changes

--- db/data.sql
+++ db/data.sql
@@ -18,9 +18,9 @@ SELECT setval('role_id_seq', (SELECT max(id) FROM role));
 
 INSERT INTO
     account
-    (id, name, username, password, role_id)
+    (id, name, role_id, username, password)
 VALUES
-    (1, 'Admin', 'admin', '$2y$10$FZSRqIGNKq64P3Rz27jlzuKuSZ9Rik9qHnqk5zH2Z7d67.erqaNhy', 1);
+    (1, 'Admin', 1, 'admin', '$2y$10$FZSRqIGNKq64P3Rz27jlzuKuSZ9Rik9qHnqk5zH2Z7d67.erqaNhy');
 
 SELECT setval('account_id_seq', (SELECT max(id) FROM account));
 
@@ -30,9 +30,9 @@ SELECT setval('account_id_seq', (SELECT max(id) FROM account));
 
 INSERT INTO
     page_content
-    (id, name, slug, menu, status, entity_id)
+    (id, name, entity_id, slug, menu, status)
 VALUES
-    (1, 'Homepage', 'index', TRUE, 'published', 'page_content');
+    (1, 'Homepage', 'page_content', 'index', TRUE, 'published');
 
 SELECT setval('page_id_seq', (SELECT max(id) FROM page));
 
