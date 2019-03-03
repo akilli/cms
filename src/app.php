8bf94e07d8b0bf5a263296025fc95fe9cc62a4b0
Ayhan Akilli <info@ayhan.biz>
2019-03-03 19:34:58 +0100

    Adjust app\allowed()

--- src/app.php
+++ src/app.php
@@ -212,7 +212,7 @@ function allowed(string $key): bool
         return false;
     }
 
-    return !$cfg['active'] || $cfg['priv'] && allowed($cfg['priv']) || account\get('admin') || in_array($key, account\get('priv'));
+    return !$cfg['active'] || account\get('admin') || $cfg['priv'] && allowed($cfg['priv']) || !$cfg['priv'] && in_array($key, account\get('priv'));
 }
 
 /**
