5df917aa7d5b96fe15d8fe9850b12652df7f708b
Ayhan Akilli <info@ayhan.biz>
2019-03-11 14:39:30 +0100

    Currently hour and minute is never needed in views

--- cfg/app.php
+++ cfg/app.php
@@ -2,7 +2,7 @@
 return [
     'admin.blacklist' => [],
     'i18n.date' => 'd.m.y',
-    'i18n.datetime' => 'd.m.y H:i',
+    'i18n.datetime' => 'd.m.y',
     'i18n.time' => 'H:i',
     'meta.description' => '',
     'meta.title' => 'akıllı CMS',
