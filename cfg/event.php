70f8fc96979343e937c957773f7c807ed8604870
Ayhan Akilli <info@ayhan.biz>
2019-04-30 13:11:24 +0200

    Move entity listener to entity namespace... for now

--- cfg/event.php
+++ cfg/event.php
@@ -25,32 +25,32 @@ return [
         'layout\listener_postrender_root' => 10,
     ],
     'entity.postvalidate' => [
-        'event\entity_postvalidate' => 10,
+        'entity\listener_postvalidate' => 10,
     ],
     'entity.prevalidate.id.file' => [
-        'event\entity_prevalidate_file' => 10,
+        'entity\listener_prevalidate_file' => 10,
     ],
     'entity.presave.id.page' => [
-        'event\entity_presave_page' => 10,
+        'entity\listener_presave_page' => 10,
     ],
     'entity.postsave.id.file' => [
-        'event\entity_postsave_file' => 10,
+        'entity\listener_postsave_file' => 10,
     ],
     'entity.postdelete.id.file' => [
-        'event\entity_postdelete_file' => 10,
+        'entity\listener_postdelete_file' => 10,
     ],
     'entity.postvalidate.id.layout' => [
-        'event\entity_postvalidate_layout' => 10,
+        'entity\listener_postvalidate_layout' => 10,
     ],
     'entity.postvalidate.id.page' => [
-        'event\entity_postvalidate_page_status' => 10,
-        'event\entity_postvalidate_page_menu' => 20,
-        'event\entity_postvalidate_page_url' => 30,
+        'entity\listener_postvalidate_page_status' => 10,
+        'entity\listener_postvalidate_page_menu' => 20,
+        'entity\listener_postvalidate_page_url' => 30,
     ],
     'entity.load.id.page' => [
-        'event\entity_load_page' => 10,
+        'entity\listener_load_page' => 10,
     ],
     'entity.predelete.id.role' => [
-        'event\entity_predelete_role' => 10,
+        'entity\listener_predelete_role' => 10,
     ],
 ];
--- src/entity.php
+++ src/entity.php
@@ -3,9 +3,12 @@ declare(strict_types = 1);
 
 namespace entity;
 
+use account;
 use arr;
 use attr;
 use app;
+use file;
+use request;
 use DomainException;
 use Throwable;
 
@@ -278,4 +281,226 @@ function event(string $name, array $data): array
     $ev[] = 'entity.' . $name . '.id.' . $entity['id'];
 
     return app\event($ev, $data);
+}
+
+/**
+ * Entity postvalidate
+ */
+function listener_postvalidate(array $data): array
+{
+    $attrs = $data['_entity']['attr'];
+
+    foreach (array_intersect_key($data, $data['_entity']['attr']) as $attrId => $val) {
+        if ($attrs[$attrId]['type'] === 'password' && $val && !($data[$attrId] = password_hash($val, PASSWORD_DEFAULT))) {
+            $data['_error'][$attrId][] = app\i18n('Invalid password');
+        }
+    }
+
+    return $data;
+}
+
+/**
+ * File entity prevalidate
+ */
+function listener_prevalidate_file(array $data): array
+{
+    if ($data['_entity']['attr']['url']['uploadable'] && !empty($data['url'])) {
+        if (!$item = request\file('url')) {
+            $data['_error']['url'][] = app\i18n('No upload file');
+        } else {
+            $data['ext'] = pathinfo($data['url'], PATHINFO_EXTENSION);
+            $data['mime'] = $item['type'];
+
+            if ($data['_old'] && ($data['ext'] !== $data['_old']['ext'] || $data['mime'] !== $data['_old']['mime'])) {
+                $data['_error']['url'][] = app\i18n('Cannot change filetype anymore');
+            }
+        }
+    }
+
+    if (!empty($data['thumb_url']) && ($item = request\file('thumb_url'))) {
+        $data['thumb_ext'] = pathinfo($data['thumb_url'], PATHINFO_EXTENSION);
+        $data['thumb_mime'] = $item['type'];
+    }
+
+    return $data;
+}
+
+/**
+ * Page entity presave
+ */
+function listener_presave_page(array $data): array
+{
+    $data['account_id'] = account\data('id');
+
+    return $data;
+}
+
+/**
+ * File entity postsave
+ *
+ * @throws DomainException
+ */
+function listener_postsave_file(array $data): array
+{
+    $id = $data['id'] ?? $data['_old']['id'] ?? null;
+    $uploadable = $data['_entity']['attr']['url']['uploadable'];
+
+    if ($uploadable && ($item = request\file('url')) && (!$id || !file\upload($item['tmp_name'], app\path('file', $id . '.' . $data['ext'])))) {
+        throw new DomainException(app\i18n('File upload failed for %s', $item['name']));
+    } elseif (($item = request\file('thumb_url')) && (!$id || !file\upload($item['tmp_name'], app\path('file', $id . APP['file.thumb'] . $data['thumb_ext'])))) {
+        throw new DomainException(app\i18n('File upload failed for %s', $item['name']));
+    }
+
+    if (array_key_exists('thumb_url', $data)
+        && !$data['thumb_url']
+        && $data['_old']['thumb_url']
+        && !file\delete(app\path('file', $data['_old']['id'] . APP['file.thumb'] . $data['_old']['thumb_ext']))
+    ) {
+        throw new DomainException(app\i18n('Could not delete file'));
+    }
+
+    return $data;
+}
+
+/**
+ * File entity postdelete
+ *
+ * @throws DomainException
+ */
+function listener_postdelete_file(array $data): array
+{
+    if ($data['_entity']['attr']['url']['uploadable']
+        && !file\delete(app\path('file', $data['_old']['id'] . '.' . $data['_old']['ext']))
+        && !file\delete(app\path('file', $data['_old']['id'] . APP['file.thumb'] . $data['_old']['thumb_ext']))
+    ) {
+        throw new DomainException(app\i18n('Could not delete file'));
+    }
+
+    return $data;
+}
+
+/**
+ * Layout entity posvalidate
+ */
+function listener_postvalidate_layout(array $data): array
+{
+    if (empty($data['name']) || empty($data['page_id']) || empty($data['parent_id'])) {
+        return $data;
+    }
+
+    $crit = [['name', $data['name']], ['page_id', $data['page_id']], ['parent_id', $data['parent_id']]];
+
+    if (!empty($data['_old']['id'])) {
+        $crit[] = ['id', $data['_old']['id'], APP['op']['!=']];
+    }
+
+    if (size('layout', $crit)) {
+        $data['_error']['name'][] = app\i18n('Name must be unique for selected parent block and page');
+    }
+
+    return $data;
+}
+
+/**
+ * Page entity postvalidate status
+ */
+function listener_postvalidate_page_status(array $data): array
+{
+    if (!empty($data['parent_id']) && ($parent = one('page', [['id', $data['parent_id']]], ['select' => ['status']]))) {
+        if ($parent['status'] === 'archived' && (!$data['_old'] || $data['parent_id'] !== $data['_old']['parent_id'])) {
+            $data['_error']['parent_id'][] = app\i18n('Cannot assign archived page as parent');
+        } elseif (in_array($parent['status'], ['draft', 'pending']) && !empty($data['status']) && $data['status'] !== 'draft') {
+            $data['_error']['status'][] = app\i18n('Status must be draft, because parent was not published yet');
+        }
+    }
+
+    return $data;
+}
+
+/**
+ * Page entity postvalidate menu
+ */
+function listener_postvalidate_page_menu(array $data): array
+{
+    if ($data['_old']
+        && !empty($data['parent_id'])
+        && ($parent = one('page', [['id', $data['parent_id']]], ['select' => ['path']]))
+        && in_array($data['_old']['id'], $parent['path'])
+    ) {
+        $data['_error']['parent_id'][] = app\i18n('Cannot assign the page itself or a subpage as parent');
+    }
+
+    return $data;
+}
+
+/**
+ * Page entity postvalidate URL
+ */
+function listener_postvalidate_page_url(array $data): array
+{
+    if ((!array_key_exists('slug', $data) || $data['_old'] && $data['slug'] === $data['_old']['slug'])
+        && (!array_key_exists('parent_id', $data) || $data['_old'] && $data['parent_id'] === $data['_old']['parent_id'])
+    ) {
+        return $data;
+    }
+
+    if (array_key_exists('slug', $data)) {
+        $slug = $data['slug'];
+    } elseif (array_key_exists('slug', $data['_old'])) {
+        $slug = $data['_old']['slug'];
+    } else {
+        $slug = null;
+    }
+
+    if (array_key_exists('parent_id', $data)) {
+        $parentId = $data['parent_id'];
+    } elseif (array_key_exists('parent_id', $data['_old'])) {
+        $parentId = $data['_old']['parent_id'];
+    } else {
+        $parentId = null;
+    }
+
+    $root = one('page', [['url', '/']], ['select' => ['id']]);
+
+    if ($parentId === null || $parentId === $root['id']) {
+        $parentId = [null, $root['id']];
+    }
+
+    $crit = [['slug', $slug], ['parent_id', $parentId]];
+
+    if ($data['_old']) {
+        $crit[] = ['id', $data['_old']['id'], APP['op']['!=']];
+    }
+
+    if (size('page', $crit)) {
+        $data['_error']['slug'][] = app\i18n('Please change slug to generate an unique URL');
+    }
+
+    return $data;
+}
+
+/**
+ * Page entity load
+ */
+function listener_load_page(array $data): array
+{
+    if (array_key_exists('content', $data)) {
+        $data['teaser'] = preg_match('#^(<p[^>]*>.*?</p>)#', trim($data['content']), $m) ? $m[1] : '';
+    }
+
+    return $data;
+}
+
+/**
+ * Role entity predelete
+ *
+ * @throws DomainException
+ */
+function listener_predelete_role(array $data): array
+{
+    if (size('account', [['role_id', $data['id']]])) {
+        throw new DomainException(app\i18n('Cannot delete used role'));
+    }
+
+    return $data;
 }
