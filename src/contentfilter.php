d0ae1a67bce5b4013cc56e857ee3e7c3a34d5241
Ayhan Akilli <info@ayhan.biz>
2019-05-14 12:47:31 +0200

    Adjust pattern

--- src/contentfilter.php
+++ src/contentfilter.php
@@ -28,7 +28,7 @@ function image(string $html, array $cfg = []): string
         return $html;
     }
 
-    $pattern = '#(<img(?:[^>]*) src="' . APP['url.file'] . '(\d+(?:\.thumb)?\.(?:jpg|png|webp))")((?:[^>]*)>)#';
+    $pattern = '#(<img(?:[^>]*) src="' . APP['url.file'] . '((\d+)(\.thumb)?\.(jpg|png|webp))")((?:[^>]*)>)#';
     $call = function (array $m) use ($cfg): string {
         $w = & app\registry('contentfilter.image');
         $w[$m[2]] = $w[$m[2]] ?? getimagesize(app\path('file', $m[2]))[0] ?: null;
@@ -46,7 +46,7 @@ function image(string $html, array $cfg = []): string
             }
         }
 
-        return $set ? $m[1] . ' srcset="' . $set . '"' . $sizes . $m[3] : $m[0];
+        return $set ? $m[1] . ' srcset="' . $set . '"' . $sizes . $m[6] : $m[0];
     };
 
     return preg_replace_callback($pattern, $call, $html);
