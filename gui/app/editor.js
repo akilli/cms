d0e682eef12984298b612b493e0b8949805e8aeb
Ayhan Akilli <info@ayhan.biz>
2019-07-30 22:57:59 +0200

    Extract observer

--- gui/app/editor.js
+++ gui/app/editor.js
@@ -1,10 +1,9 @@
 import Editor from '../editor/src/Editor.js';
-import EditorConfig from '../editor/src/config/EditorConfig.js';
 import app from './app.js';
 
 /**
  * Editor
  */
 export default function () {
-    document.addEventListener('DOMContentLoaded', () => document.querySelectorAll('textarea[data-type=editor]').forEach(item => Editor.create(item, new EditorConfig(app.cfg.editor))));
+    document.addEventListener('DOMContentLoaded', () => document.querySelectorAll('textarea[data-type=editor]').forEach(item => Editor.create(item, app.cfg.editor)));
 }
