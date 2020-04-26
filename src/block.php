1526f566bb3a336f6a1a189fd0759f9e82c34f58
Ayhan Akilli <info@ayhan.biz>
2020-04-26 10:47:22 +0200

    Clean up

--- src/block.php
+++ src/block.php
@@ -33,7 +33,7 @@ function container(array $block): string
 function html(): string
 {
     $app = app\data('app');
-    $a = ['lang' => APP['lang'], 'data-action' => $app['action'], 'data-entity' => $app['entity_id'], 'data-parent' => $app['parent_id'], 'data-url' => app\data('request', 'url')];
+    $a = ['lang' => APP['lang'], 'data-parent' => $app['parent_id'], 'data-entity' => $app['entity_id'], 'data-action' => $app['action'], 'data-url' => app\data('request', 'url')];
 
     return "<!doctype html>\n" . app\html('html', $a, layout\block('head') . layout\block('body'));
 }
