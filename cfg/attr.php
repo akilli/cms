d64cc42ed1bf0479b65c1fe7358bf41a777c2a76
Ayhan Akilli <info@ayhan.biz>
2020-06-24 14:48:40 +0200

    Upgrade editor to v0.87.0 and adjust editor content filter rules

--- cfg/attr.php
+++ cfg/attr.php
@@ -49,7 +49,7 @@ return [
         'filter' => 'frontend\text',
         'validator' => 'validator\editor',
         'viewer' => 'viewer\editor',
-        'cfg.validator' => '<a><abbr><article><audio><b><blockquote><br><cite><code><data><details><dfn><div><editor-block><em><figcaption><figure><h1><h2><h3><h4><h5><h6><i><iframe><img><kbd><li><mark><ol><p><q><s><section><samp><small><strong><sub><summary><sup><table><tbody><td><tfoot><th><thead><time><tr><u><ul><var><video>',
+        'cfg.validator' => '<a><abbr><article><audio><b><blockquote><br><cite><code><data><del><details><dfn><div><editor-block><em><figcaption><figure><h1><h2><h3><h4><h5><h6><hr><i><iframe><img><ins><kbd><li><mark><ol><p><pre><q><s><section><samp><small><strong><sub><summary><sup><table><tbody><td><tfoot><th><thead><time><tr><u><ul><var><video>',
     ],
     'email' => [
         'backend' => 'varchar',
