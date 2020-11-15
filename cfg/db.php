e082e4d2c4d8b7d2d80f9bb3d6997aa1c86fd097
Ayhan Akilli <info@ayhan.biz>
2020-11-15 14:25:45 +0100

    Split db config into separate files

--- cfg/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'app' => [
-        'type' => 'sql',
-        'dsn' => 'pgsql:host=db;dbname=app',
-        'user' => 'app',
-        'password' => 'app',
-    ],
-];
---
+++ cfg/db/app.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'type' => 'sql',
+    'dsn' => 'pgsql:host=db;dbname=app',
+    'user' => 'app',
+    'password' => 'app',
+];
