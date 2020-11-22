3e6ddb6c594bb0e5416639225682810f45d8e924
Ayhan Akilli <info@ayhan.biz>
2020-11-22 14:55:35 +0100

    Use match expression

--- src/block.php
+++ src/block.php
@@ -60,14 +60,11 @@ function msg(): string
 function title(array $block): string
 {
     $app = app\data('app');
-
-    if ($block['cfg']['text']) {
-        $text = app\i18n($block['cfg']['text']);
-    } elseif ($app['area'] === '_public_') {
-        $text = $app['page']['title'] ?? $app['page']['name'] ?? '';
-    } else {
-        $text = $app['entity']['name'] ?? '';
-    }
+    $text = match (true) {
+        $block['cfg']['text'] => app\i18n($block['cfg']['text']),
+        $app['area'] === '_public_' => $app['page']['title'] ?? $app['page']['name'] ?? '',
+        default => $app['entity']['name'] ?? '',
+    };
 
     return $text ? app\html('h1', [], str\enc($text)) : '';
 }
