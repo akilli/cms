737552a418f1f7d0193535e21797719f3ef7f0d4
Ayhan Akilli <info@ayhan.biz>
2020-12-03 01:20:14 +0100

    APP['crlf']

--- src/smtp.php
+++ src/smtp.php
@@ -97,7 +97,7 @@ function receive($client): ?string
  */
 function send($client, string $msg, array $status): void
 {
-    fputs($client, $msg . "\r\n");
+    fputs($client, $msg . APP['crlf']);
     $data = receive($client);
 
     if (!$data || !preg_match('#^([1-5][0-9][0-9])(?:.*)$#', $data, $match) || !in_array((int) $match[1], $status)) {
