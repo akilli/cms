9a477ec385b4e4a2c8b0f5a2d462788b5249403d
Ayhan Akilli <info@ayhan.biz>
2020-12-20 12:19:30 +0100

    Cleanup

--- cfg/toolbar.php
+++ cfg/toolbar.php
@@ -1,38 +1,12 @@
 <?php
 return [
-    'home' => [
-        'name' => 'Homepage',
-        'url' => '/',
-        'sort' => 100,
-    ],
-    'page' => [
-        'sort' => 200,
-    ],
-    'layout' => [
-        'sort' => 300,
-    ],
-    'block' => [
-        'sort' => 400,
-    ],
-    'file' => [
-        'sort' => 500,
-    ],
-    'role' => [
-        'sort' => 600,
-    ],
-    'account' => [
-        'sort' => 700,
-    ],
-    'profile' => [
-        'name' => 'Profile',
-        'privilege' => 'account:profile',
-        'url' => '/account/profile',
-        'sort' => 800,
-    ],
-    'logout' => [
-        'name' => 'Logout',
-        'privilege' => 'account:logout',
-        'url' => '/account/logout',
-        'sort' => 900,
-    ],
+    'home' => ['name' => 'Homepage', 'url' => '/', 'sort' => 100],
+    'page' => ['sort' => 200],
+    'layout' => ['sort' => 300],
+    'block' => ['sort' => 400],
+    'file' => ['sort' => 500],
+    'role' => ['sort' => 600],
+    'account' => ['sort' => 700],
+    'profile' => ['name' => 'Profile', 'privilege' => 'account:profile', 'url' => '/account/profile', 'sort' => 800],
+    'logout' => ['name' => 'Logout', 'privilege' => 'account:logout', 'url' => '/account/logout', 'sort' => 900],
 ];
