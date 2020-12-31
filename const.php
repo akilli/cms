17530a16fbe86835f97d0d8d40fe5e9263e16e7b
Ayhan Akilli <info@ayhan.biz>
2020-12-31 01:02:36 +0100

    Cleanup

--- const.php
+++ const.php
@@ -29,12 +29,7 @@ define('APP', [
             'pattern' => null,
             'accept' => [],
         ],
-        'block' => [
-            'call' => null,
-            'tpl' => null,
-            'tag' => null,
-            'cfg' => [],
-        ],
+        'block' => ['call' => null, 'tpl' => null, 'tag' => null, 'cfg' => []],
         'entity' => [
             'id' => null,
             'name' => null,
@@ -46,11 +41,7 @@ define('APP', [
             'action' => [],
             'attr' => [],
         ],
-        'privilege' => [
-            'name' => null,
-            'auto' => false,
-            'use' => null,
-        ],
+        'privilege' => ['name' => null, 'auto' => false, 'use' => null],
         'toolbar' => [
             'id' => null,
             'name' => null,
@@ -108,20 +99,9 @@ define('APP', [
     'datetime.frontend' => 'Y-m-d\TH:i',
     'html.tags' => '<a><abbr><article><audio><b><blockquote><br><cite><code><data><del><details><dfn><div><em><figcaption><figure><h1><h2><h3><h4><h5><h6><hr><i><iframe><img><ins><kbd><li><mark><ol><p><pre><q><s><section><samp><small><strong><sub><summary><sup><table><tbody><td><tfoot><th><thead><time><tr><u><ul><var><video><app-block>',
     'html.void' => ['area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 'link', 'meta', 'param', 'source', 'track', 'wbr'],
-    'image' => [
-        'srcset' => [360, 640, 800, 960, 1120, 1280, 1440],
-        'sizes' => null,
-        'thumb' => 879,
-    ],
+    'image' => ['srcset' => [360, 640, 800, 960, 1120, 1280, 1440], 'sizes' => null, 'thumb' => 879],
     'image.ext' => ['jpg', 'png', 'webp'],
-    'join' => [
-        'cross' => 'CROSS',
-        'full' => 'FULL',
-        'inner' => 'INNER',
-        'left' => 'LEFT',
-        'natural' => 'NATURAL',
-        'right' => 'RIGHT'
-    ],
+    'join' => ['cross' => 'CROSS', 'full' => 'FULL', 'inner' => 'INNER', 'left' => 'LEFT', 'natural' => 'NATURAL', 'right' => 'RIGHT'],
     'lang' => locale_get_primary_language(''),
     'locale' => ini_get('intl.default_locale'),
     'log' => 'php://stdout',
@@ -151,27 +131,11 @@ define('APP', [
         'file' => '/data/file/',
         'tmp' => '/tmp/',
     ],
-    'pdo' => [
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ],
-    'response' => [
-        'body' => '',
-        'redirect' => null,
-    ],
+    'pdo' => [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION],
+    'response' => ['body' => '', 'redirect' => null],
     'time.backend' => 'H:i:s',
     'time.frontend' => 'H:i',
     'upload' => ['error', 'name', 'size', 'tmp_name', 'type'],
-    'url' => [
-        'ext' => '/ext/',
-        'file' => '/file/',
-        'gui' => '/gui/',
-    ],
-    'viewer' => [
-        'empty' => false,
-        'label' => false,
-        'link' => null,
-        'subheading' => false,
-        'wrap' => false,
-    ],
+    'url' => ['ext' => '/ext/', 'file' => '/file/', 'gui' => '/gui/'],
+    'viewer' => ['empty' => false, 'label' => false, 'link' => null, 'subheading' => false, 'wrap' => false],
 ]);
