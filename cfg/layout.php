855d2c7732b4950886dd2e508f24ce2e227f3941
Ayhan Akilli <info@ayhan.biz>
2021-08-12 18:52:28 +0200

    Remove footer

--- cfg/layout.php
+++ cfg/layout.php
@@ -43,7 +43,6 @@ return [
         'header' => ['type' => 'tpl', 'tpl' => 'header.phtml', 'parent_id' => 'body', 'sort' => 140],
         'menu' => ['type' => 'menu', 'parent_id' => 'body', 'sort' => 160, 'cfg' => ['toggle' => true]],
         'breadcrumb' => ['type' => 'breadcrumb', 'parent_id' => 'main', 'sort' => 50],
-        'footer' => ['type' => 'tpl', 'tpl' => 'footer.phtml', 'parent_id' => 'body', 'sort' => 300],
     ],
     'html:account:dashboard' => [
         'title' => ['cfg' => ['text' => 'Dashboard']],
