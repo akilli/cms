4855f0eaef717c44f99b3178eeb5d2da3a0b997a
Ayhan Akilli <info@ayhan.biz>
2021-10-07 10:19:02 +0200

    Merge admin.js into all.js and admin.css into all.css for now, will be reorganized from scratch

--- gui/all.js
+++ gui/all.js
@@ -1,5 +1,19 @@
-import print from './app/print.js';
+import browser from './app/browser.js';
+import confirmdelete from './app/confirmdelete.js';
+import datetime from './polyfill/datetime.js';
+import editor from './app/editor.js';
+import invalid from './app/invalid.js';
+import multicheckbox from './app/multicheckbox.js';
 import nav from './app/nav.js';
+import print from './app/print.js';
+import privilege from './app/privilege.js';
 
 print();
 nav();
+datetime();
+confirmdelete();
+multicheckbox();
+privilege();
+invalid();
+editor();
+window.opener ? browser.win() : browser.open();
