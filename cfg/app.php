705294aadf86d90d6d31fdfe90ce5fe903587a6c
Ayhan Akilli <info@ayhan.biz>
2021-10-19 09:43:27 +0200

    Adjust datetime format

--- cfg/app.php
+++ cfg/app.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 return [
     'date' => 'dd.MM.yyyy',
-    'datetime' => 'dd.MM.yyyy HH:mm',
+    'datetime' => 'dd.MM.yyyy, HH:mm',
     'time' => 'HH:mm',
     'title' => 'akıllı CMS',
 ];
