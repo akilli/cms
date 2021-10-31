85f01ea2cf022cb63c11f636f703464042d7ccc2
Ayhan Akilli <info@ayhan.biz>
2021-10-31 23:00:22 +0100

    Adjust styles X

--- cfg/layout/html.php
+++ cfg/layout/html.php
@@ -86,6 +86,11 @@ return [
         'parent_id' => 'header',
         'sort' => 100,
     ],
+    'title' => [
+        'type' => 'title',
+        'parent_id' => 'header',
+        'sort' => 200,
+    ],
     'content' => [
         'type' => 'container',
         'tag' => 'article',
@@ -103,11 +108,6 @@ return [
         'cfg' => [
             'id' => true,
         ],
-    ],
-    'title' => [
-        'type' => 'title',
-        'parent_id' => 'content',
-        'sort' => 100,
     ],
     'msg' => [
         'type' => 'tag',
