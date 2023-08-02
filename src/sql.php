7bd9319837dfa8af8c38ec89d211ca1a6b067066
Ayhan Akilli <info@ayhan.biz>
2023-08-02 20:26:24 +0200

    Extend sql functions

--- src/sql.php
+++ src/sql.php
@@ -3,36 +3,21 @@ declare(strict_types=1);
 
 namespace sql;
 
-use app;
-use DomainException;
-
-/**
- * INSERT part
- */
 function insert(string $table): string
 {
     return 'INSERT INTO ' . $table;
 }
 
-/**
- * VALUES part
- */
 function values(array $cols): string
 {
     return ' (' . implode(', ', array_keys($cols)) . ') VALUES (' . implode(', ', $cols) . ')';
 }
 
-/**
- * UPDATE part
- */
 function update(string $table): string
 {
     return 'UPDATE ' . $table;
 }
 
-/**
- * SET part
- */
 function set(array $cols): string
 {
     $sql = '';
@@ -44,17 +29,11 @@ function set(array $cols): string
     return ' SET ' . $sql;
 }
 
-/**
- * DELETE part
- */
 function delete(string $table): string
 {
     return 'DELETE FROM ' . $table;
 }
 
-/**
- * SELECT part
- */
 function select(array $sel): string
 {
     $sql = '';
@@ -66,49 +45,61 @@ function select(array $sel): string
     return $sql ? ' SELECT ' . $sql : '';
 }
 
-/**
- * FROM part
- */
 function from(string $table, string $as = null): string
 {
     return ' FROM ' . $table . ($as ? ' AS ' . $as : '');
 }
 
-/**
- * WHERE part
- */
 function where(array $cols): string
 {
     return $cols ? ' WHERE ' . implode(' AND ', $cols) : '';
 }
 
-/**
- * JOIN part
- *
- * @throws DomainException
- */
-function join(string $type, string $table, string $as = null, array $cols = []): string
+function natural_join(string $table, string $as = null, array $cols = []): string
 {
-    if (empty(APP['join'][$type]) || !$table) {
-        throw new DomainException(app\i18n('Invalid JOIN'));
-    }
-
     $on = $cols ? ' ON ' . implode(' AND ', $cols) : '';
 
-    return APP['join'][$type] . ' JOIN ' . $table . ($as ? ' AS ' . $as : '') . $on;
+    return ' NATURAL JOIN ' . $table . ($as ? ' AS ' . $as : '') . $on;
 }
 
-/**
- * GROUP BY part
- */
+function inner_join(string $table, string $as = null, array $cols = []): string
+{
+    $on = $cols ? ' ON ' . implode(' AND ', $cols) : '';
+
+    return ' INNER JOIN ' . $table . ($as ? ' AS ' . $as : '') . $on;
+}
+
+function left_join(string $table, string $as = null, array $cols = []): string
+{
+    $on = $cols ? ' ON ' . implode(' AND ', $cols) : '';
+
+    return ' LEFT JOIN ' . $table . ($as ? ' AS ' . $as : '') . $on;
+}
+
+function right_join(string $table, string $as = null, array $cols = []): string
+{
+    $on = $cols ? ' ON ' . implode(' AND ', $cols) : '';
+
+    return ' RIGHT JOIN ' . $table . ($as ? ' AS ' . $as : '') . $on;
+}
+
+function full_join(string $table, string $as = null, array $cols = []): string
+{
+    $on = $cols ? ' ON ' . implode(' AND ', $cols) : '';
+
+    return ' FULL JOIN ' . $table . ($as ? ' AS ' . $as : '') . $on;
+}
+
+function cross_join(string $table, string $as = null): string
+{
+    return ' CROSS JOIN ' . $table . ($as ? ' AS ' . $as : '');
+}
+
 function group(array $cols): string
 {
     return $cols ? ' GROUP BY ' . implode(', ', $cols) : '';
 }
 
-/**
- * ORDER BY part
- */
 function order(array $order): string
 {
     $sql = '';
@@ -120,17 +111,11 @@ function order(array $order): string
     return $sql ? ' ORDER BY ' . $sql : '';
 }
 
-/**
- * LIMIT part
- */
 function limit(int $limit, int $offset = 0): string
 {
     return $limit > 0 ? ' LIMIT ' . $limit . ' OFFSET ' . max(0, $offset) : '';
 }
 
-/**
- * WITH part
- */
 function with(array $with, bool $recursive = false): string
 {
     $sql = '';
@@ -142,17 +127,11 @@ function with(array $with, bool $recursive = false): string
     return 'WITH ' . ($recursive ? 'RECURSIVE ' : '') . $sql;
 }
 
-/**
- * RETURNING part
- */
 function returning(array $cols): string
 {
     return $cols ? ' RETURNING ' . implode(', ', $cols) : '';
 }
 
-/**
- * UNION part
- */
 function union(): string
 {
     return ' UNION ';
